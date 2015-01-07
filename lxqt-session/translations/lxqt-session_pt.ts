2011d0013fbcaaac3774b1a33714382c2d30c944
Sérgio Marques <smarquespt@gmail.com>
2015-01-07 14:00:45 +0000

    Portuguese update

--- lxqt-session/translations/lxqt-session_pt.ts
+++ lxqt-session/translations/lxqt-session_pt.ts
@@ -1,44 +1,44 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt">
+<TS version="2.0" language="pt">
 <context>
     <name>LxQtModuleManager</name>
     <message>
         <source>LxQt Session Crash Report</source>
-        <translation type="vanished">Relatório de erros da sessão LxQt</translation>
+        <translation>Relatório de erros do LxQt</translation>
     </message>
     <message>
         <source>Application &apos;%1&apos; crashed too many times. Its autorestart has been disabled for current session.</source>
-        <translation type="vanished">A aplicação &apos;%1&apos; terminou muitas vezes. O seu arranque automático foi desativado nesta sessão.</translation>
+        <translation>A aplicação &apos;%1&apos; terminou muitas vezes. O seu arranque automático foi desativado nesta sessão.</translation>
     </message>
     <message>
         <location filename="../src/lxqtmodman.cpp" line="289"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Relatório de erros</translation>
     </message>
     <message>
         <location filename="../src/lxqtmodman.cpp" line="290"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; terminou muitas vezes. O seu arranque automático foi desativado até à próxima sessão.</translation>
     </message>
 </context>
 <context>
     <name>WmSelectDialog</name>
     <message>
         <source>Welcome to LXDE-Qt</source>
-        <translation type="vanished">Bem-vindo ao LXDE-Qt</translation>
+        <translation>Bem-vindo ao LxQt</translation>
     </message>
     <message>
         <source>&lt;b&gt;Welcome to LXDE-Qt&lt;/b&gt;
 &lt;p&gt;
 Before starting to use the LXDE-Qt, you might want to select the Windows Manager:</source>
-        <translation type="vanished">&lt;b&gt;Bem-vindo ao LXDE-Qt&lt;/b&gt;
+        <translation>&lt;b&gt;Bem-vindo ao LxQt&lt;/b&gt;
 &lt;p&gt;
-Antes de começar a utilizar o LXDE-Qt, deve escolher o gestor de janelas:</translation>
+Antes de começar a utilizar o LxQt, deve escolher o seu gestor de janelas:</translation>
     </message>
     <message>
         <source>You can change the Window Manager later at any time via LxQt Session Configurator.</source>
-        <translation type="vanished">Posteriormente, se quiser, pode alterar o gestor de janelas através do configurador de sessões do LxQt.</translation>
+        <translation>Posteriormente, pode mudar o gestor de janelas através do configurador de sessões do LxQt.</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.cpp" line="62"/>
@@ -53,17 +53,17 @@ Antes de começar a utilizar o LXDE-Qt, deve escolher o gestor de janelas:</tran
     <message>
         <location filename="../src/wmselectdialog.ui" line="14"/>
         <source>Welcome to LXQt</source>
-        <translation type="unfinished"></translation>
+        <translation>Bem-vindo ao LxQt</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Bem-vindo ao LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Por favor escolha o seu gestor de janelas.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="71"/>
         <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Posteriormente, pode mudar o gestor de janelas em Preferências -&gt; Definiçoes da sessão -&gt; Definições básicas.</translation>
     </message>
 </context>
 </TS>
