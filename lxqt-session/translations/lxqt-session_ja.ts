1889e24e2be93b6120dd6282616f51896b2bafab
Mika Kobayashi <mika-k@users.noreply.github.com>
2015-01-18 20:33:41 -0500

    Update Japanese translation
    - Add and update Japanese translated files.
    
    	modified:   lxqt-config-session/translations/lxqt-config-session_ja.desktop
    	modified:   lxqt-config-session/translations/lxqt-config-session_ja.ts
    	new file:   lxqt-session/translations/lxqt-session_ja.ts
    
    - Add Japanese translated lxqtleave's desktop files.
    
    	new file:   lxqtleave/translations/lxqt-hibernate_ja.desktop
    	new file:   lxqtleave/translations/lxqt-lockscreen_ja.desktop
    	new file:   lxqtleave/translations/lxqt-logout_ja.desktop
    	new file:   lxqtleave/translations/lxqt-reboot_ja.desktop
    	new file:   lxqtleave/translations/lxqt-shutdown_ja.desktop
    	new file:   lxqtleave/translations/lxqt-suspend_ja.desktop
    
    - Remove *ja_JP.ts files.
    
      Because Japanese doesn't have any other country-specific flavour,
     *ja_JP.ts files are duplicated and useless.
    
    	deleted:    lxqt-config-session/translations/lxqt-config-session_ja_JP.ts
    	deleted:    lxqt-session/translations/lxqt-session_ja_JP.ts

--- lxqt-session/translations/lxqt-session_ja.ts
+++ lxqt-session/translations/lxqt-session_ja.ts
@@ -4,41 +4,18 @@
 <context>
     <name>LxQtModuleManager</name>
     <message>
-        <source>LxQt Session Crash Report</source>
-        <translation type="vanished">LxQtセッションのクラッシュ報告</translation>
-    </message>
-    <message>
-        <source>Application &apos;%1&apos; crashed too many times. Its autorestart has been disabled for current session.</source>
-        <translation type="vanished">アプリケーション&apos;%1&apos;はクラッシュしすぎました。現在のセッションにおける自動起動は無効化されました。</translation>
-    </message>
-    <message>
         <location filename="../src/lxqtmodman.cpp" line="289"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>クラッシュ報告</translation>
     </message>
     <message>
         <location filename="../src/lxqtmodman.cpp" line="290"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt;は何度もクラッシュしましたので、次回のログインまで自動起動を無効にしました</translation>
     </message>
 </context>
 <context>
     <name>WmSelectDialog</name>
-    <message>
-        <source>Welcome to LXDE-Qt</source>
-        <translation type="vanished">LXDE-Qtにようこそ</translation>
-    </message>
-    <message>
-        <source>&lt;b&gt;Welcome to LXDE-Qt&lt;/b&gt;
-&lt;p&gt;
-Before starting to use the LXDE-Qt, you might want to select the Windows Manager:</source>
-        <translation type="vanished">&lt;b&gt;LXDE-Qtへようこそ&lt;/b&gt;
-&lt;p&gt;LXDE-Qtを使い始める前に、ウィンドウマネージャを選択することもできます:</translation>
-    </message>
-    <message>
-        <source>You can change the Window Manager later at any time via LxQt Session Configurator.</source>
-        <translation type="vanished">ウィンドウマネージャは後からいつでもLxQtセッション設定で変更することができます。</translation>
-    </message>
     <message>
         <location filename="../src/wmselectdialog.cpp" line="62"/>
         <source>Other ...</source>
@@ -47,22 +24,22 @@ Before starting to use the LXDE-Qt, you might want to select the Windows Manager
     <message>
         <location filename="../src/wmselectdialog.cpp" line="63"/>
         <source>Choose your favorite one.</source>
-        <translation>お好みのものを選んでください。</translation>
+        <translation>お好みのものを選んでください</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="14"/>
         <source>Welcome to LXQt</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">LXQtにようこそ</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="20"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;LXQtにようこそ&lt;/span&gt;&lt;/p&gt;&lt;p&gt;デフォルトのウィンドウマネージャーを選択してください&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="71"/>
         <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>この設定はいつでも、設定 -&gt; セッションの設定 -&gt; 基本設定 で変更することができます</translation>
     </message>
 </context>
 </TS>
