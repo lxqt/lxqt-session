452dca312464e84458935ff69b8e9e774c484f0b
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-05-14 22:54:15 +0200

    Removed empty german translations, moved and updated translations from Germany to common german.

--- lxqt-config-session/translations/lxqt-config-session_de.ts
+++ lxqt-config-session/translations/lxqt-config-session_de.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de">
+<TS version="2.1" language="de_DE">
 <context>
     <name>AutoStartEdit</name>
     <message>
         <location filename="../autostartedit.ui" line="14"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Bearbeiten</translation>
     </message>
     <message>
         <location filename="../autostartedit.ui" line="20"/>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Name:</translation>
     </message>
     <message>
         <location filename="../autostartedit.ui" line="27"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche...</translation>
     </message>
     <message>
         <location filename="../autostartedit.ui" line="47"/>
         <source>Command:</source>
-        <translation type="unfinished"></translation>
+        <translation>Befehl:</translation>
     </message>
     <message>
         <location filename="../autostartedit.ui" line="57"/>
         <source>Wait for system tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Warte auf System Tray</translation>
     </message>
     <message>
         <location filename="../autostartedit.cpp" line="59"/>
         <source>Select Application</source>
-        <translation type="unfinished"></translation>
+        <translation>Anwendung auswählen</translation>
     </message>
 </context>
 <context>
@@ -39,7 +39,11 @@
     <message>
         <location filename="../autostartmodel.cpp" line="177"/>
         <source>Global Autostart</source>
-        <translation type="unfinished"></translation>
+        <translation>Globaler Autostart</translation>
+    </message>
+    <message>
+        <source>LxQt Autostart</source>
+        <translation type="vanished">LxQt Autostart</translation>
     </message>
     <message>
         <location filename="../autostartmodel.cpp" line="179"/>
@@ -49,12 +53,12 @@
     <message>
         <location filename="../autostartmodel.cpp" line="197"/>
         <source>Location: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ort: %1</translation>
     </message>
     <message>
         <location filename="../autostartmodel.cpp" line="199"/>
         <source>Overrides: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Überschreibung: %1</translation>
     </message>
 </context>
 <context>
@@ -62,32 +66,32 @@
     <message>
         <location filename="../autostartpage.ui" line="17"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Löschen</translation>
     </message>
     <message>
         <location filename="../autostartpage.ui" line="24"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Hinzufügen</translation>
     </message>
     <message>
         <location filename="../autostartpage.ui" line="31"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Bearbeiten</translation>
     </message>
     <message>
         <location filename="../autostartpage.ui" line="44"/>
         <source>Application Autostart</source>
-        <translation type="unfinished"></translation>
+        <translation>Anwendung-Autostart</translation>
     </message>
     <message>
         <location filename="../autostartpage.cpp" line="82"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler</translation>
     </message>
     <message>
         <location filename="../autostartpage.cpp" line="82"/>
         <source>File &apos;%1&apos; already exists!</source>
-        <translation type="unfinished"></translation>
+        <translation>Datei &apos;%1&apos; existiert bereits!</translation>
     </message>
 </context>
 <context>
@@ -95,26 +99,16 @@
     <message>
         <location filename="../basicsettings.ui" line="30"/>
         <source>Window Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Fenstermanager</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="49"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Suchen...</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="59"/>
         <source>LXQt Modules</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../basicsettings.ui" line="65"/>
-        <source>Start</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../basicsettings.ui" line="72"/>
-        <source>Stop</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -130,12 +124,26 @@
     <message>
         <location filename="../basicsettings.ui" line="23"/>
         <source>Basic Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Grundeinstellungen</translation>
+    </message>
+    <message>
+        <source>LxQt Modules</source>
+        <translation type="vanished">LxQt-Module</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="65"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="72"/>
+        <source>Stop</source>
+        <translation>Stopp</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="84"/>
         <source>Select a window manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Fenstermanager auswählen</translation>
     </message>
 </context>
 <context>
@@ -143,12 +151,12 @@
     <message>
         <location filename="../defaultappspage.cpp" line="72"/>
         <source>Select a terminal emulator</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminal-Emulator auswählen</translation>
     </message>
     <message>
         <location filename="../defaultappspage.cpp" line="77"/>
         <source>Select a web browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Web Browser auswählen</translation>
     </message>
 </context>
 <context>
@@ -156,23 +164,23 @@
     <message>
         <location filename="../defaultappspage.ui" line="23"/>
         <source>Default Applications</source>
-        <translation type="unfinished"></translation>
+        <translation>Standardanwendungen</translation>
     </message>
     <message>
         <location filename="../defaultappspage.ui" line="32"/>
         <source>Terminal Emulation:</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminal-Emulator:</translation>
     </message>
     <message>
         <location filename="../defaultappspage.ui" line="52"/>
         <location filename="../defaultappspage.ui" line="79"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche...</translation>
     </message>
     <message>
         <location filename="../defaultappspage.ui" line="59"/>
         <source>Web Browser:</source>
-        <translation type="unfinished"></translation>
+        <translation>Web Browser:</translation>
     </message>
 </context>
 <context>
@@ -180,27 +188,27 @@
     <message>
         <location filename="../environmentpage.ui" line="30"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Hinzufügen</translation>
     </message>
     <message>
         <location filename="../environmentpage.ui" line="37"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Löschen</translation>
     </message>
     <message>
         <location filename="../environmentpage.ui" line="63"/>
         <source>Environment Variables (Advanced)</source>
-        <translation type="unfinished"></translation>
+        <translation>Umgebungsvariablen (Erweitert)</translation>
     </message>
     <message>
         <location filename="../environmentpage.ui" line="74"/>
         <source>Variable Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Variablenname</translation>
     </message>
     <message>
         <location filename="../environmentpage.ui" line="79"/>
         <source>Value</source>
-        <translation type="unfinished"></translation>
+        <translation>Wert</translation>
     </message>
 </context>
 <context>
@@ -208,11 +216,15 @@
     <message>
         <location filename="../modulemodel.cpp" line="85"/>
         <source>Running</source>
-        <translation type="unfinished"></translation>
+        <translation>Läuft</translation>
     </message>
 </context>
 <context>
     <name>SessionConfigWindow</name>
+    <message>
+        <source>LxQt Session Configuration</source>
+        <translation type="vanished">LxQt Sitzungs-Konfiguration</translation>
+    </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="43"/>
         <source>LXQt Session Settings</source>
@@ -221,32 +233,32 @@
     <message>
         <location filename="../sessionconfigwindow.cpp" line="46"/>
         <source>Basic Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Grundeinstellungen</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="52"/>
         <source>Default Applications</source>
-        <translation type="unfinished"></translation>
+        <translation>Standardanwendungen</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="55"/>
         <source>Autostart</source>
-        <translation type="unfinished"></translation>
+        <translation>Autostart</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="60"/>
         <source>Environment (Advanced)</source>
-        <translation type="unfinished"></translation>
+        <translation>Umgebung (Erweitert)</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="84"/>
         <source>Session Restart Required</source>
-        <translation type="unfinished"></translation>
+        <translation>Erneute Anmeldung erforderlich</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="85"/>
         <source>You need to restart desktop session (lxqt-session) to reload settings. Use logout from the main menu.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie müssen sich erneut anmelden (lxqt-session), damit Ihre Änderungen aktiv werden. Klicken Sie dafür auf &quot;Abmelden&quot; im Hauptmenü.</translation>
     </message>
 </context>
 </TS>
