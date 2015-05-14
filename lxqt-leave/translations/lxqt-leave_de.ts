9934449e94bb44c700167edb79c67278e3cb4ee5
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-05-14 23:06:23 +0200

    Added german translation for the leave form.

---
+++ lxqt-leave/translations/lxqt-leave_de.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Form</source>
+        <translation>Verlassen</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="29"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Was soll der Computer tun?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="45"/>
+        <source>Logout</source>
+        <translation>Abmelden</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="55"/>
+        <source>Reboot</source>
+        <translation>Neustart</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="65"/>
+        <source>Shutdown</source>
+        <translation>Herunterfahren</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="75"/>
+        <source>Suspend</source>
+        <translation>Bereitschaft</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="85"/>
+        <source>Hibernate</source>
+        <translation>Tiefschlaf</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="114"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="51"/>
+        <source>Logout.</source>
+        <translation>Abmelden.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="54"/>
+        <source>Lockscreen.</source>
+        <translation>Sperren.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="57"/>
+        <source>Suspend.</source>
+        <translation>Bereitschaft.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Hibernate.</source>
+        <translation>Tiefschlaf.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="63"/>
+        <source>Shutdown.</source>
+        <translation>Herunterfahren.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="66"/>
+        <source>Reboot.</source>
+        <translation>Neustart.</translation>
+    </message>
+</context>
+</TS>
