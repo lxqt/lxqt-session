a3eb930e53b81b8894f6696881f05d44c53a9547
stefonarch <stef.binde@riseup.net>
2015-06-02 15:42:03 +0200

    Completed italian translation

---
+++ lxqt-leave/translations/lxqt-leave_it.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="29"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="45"/>
+        <source>Logout</source>
+        <translation>Logout</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="55"/>
+        <source>Reboot</source>
+        <translation>Riavvia</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="65"/>
+        <source>Shutdown</source>
+        <translation>Spegni</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="75"/>
+        <source>Suspend</source>
+        <translation>Sospendi</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="85"/>
+        <source>Hibernate</source>
+        <translation>Iberna</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="114"/>
+        <source>Cancel</source>
+        <translation>Cancella</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="51"/>
+        <source>Logout.</source>
+        <translation>Logout.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="54"/>
+        <source>Lockscreen.</source>
+        <translation>Blocca schermo.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="57"/>
+        <source>Suspend.</source>
+        <translation>Sospendi.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Hibernate.</source>
+        <translation>Iberna.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="63"/>
+        <source>Shutdown.</source>
+        <translation>Spegni.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="66"/>
+        <source>Reboot.</source>
+        <translation>Riavvia.</translation>
+    </message>
+</context>
+</TS>
