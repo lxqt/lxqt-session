bc3517132c0bd3af9e1d16a96461d7e416ca1d27
Dimitrios Glentadakis <dglent@free.fr>
2015-08-25 05:55:01 +0200

    Update Greek (el) translation

--- lxqt-config-session/translations/lxqt-config-session_el_GR.ts
+++ lxqt-config-session/translations/lxqt-config-session_el_GR.ts
@@ -1,35 +1,35 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el_GR">
 <context>
     <name>AutoStartEdit</name>
     <message>
-        <location filename="../autostartedit.ui" line="14"/>
+        <location line="14" filename="../autostartedit.ui"/>
         <source>Edit</source>
         <translation>Επεξεργασία</translation>
     </message>
     <message>
-        <location filename="../autostartedit.ui" line="20"/>
+        <location line="20" filename="../autostartedit.ui"/>
         <source>Name:</source>
         <translation>Όνομα:</translation>
     </message>
     <message>
-        <location filename="../autostartedit.ui" line="27"/>
+        <location line="27" filename="../autostartedit.ui"/>
         <source>Search...</source>
         <translation>Αναζήτηση...</translation>
     </message>
     <message>
-        <location filename="../autostartedit.ui" line="47"/>
+        <location line="47" filename="../autostartedit.ui"/>
         <source>Command:</source>
         <translation>Εντολή:</translation>
     </message>
     <message>
-        <location filename="../autostartedit.ui" line="57"/>
+        <location line="57" filename="../autostartedit.ui"/>
         <source>Wait for system tray</source>
         <translation>Αναμονή για το πλαίσιο συστήματος</translation>
     </message>
     <message>
-        <location filename="../autostartedit.cpp" line="59"/>
+        <location line="59" filename="../autostartedit.cpp"/>
         <source>Select Application</source>
         <translation>Επιλογή εφαρμογής</translation>
     </message>
@@ -37,7 +37,7 @@
 <context>
     <name>AutoStartItemModel</name>
     <message>
-        <location filename="../autostartmodel.cpp" line="177"/>
+        <location line="177" filename="../autostartmodel.cpp"/>
         <source>Global Autostart</source>
         <translation>Καθολική αυτόματη εκκίνηση</translation>
     </message>
@@ -46,17 +46,17 @@
         <translation type="vanished">Αυτόματη εκκίνηση LxQt</translation>
     </message>
     <message>
-        <location filename="../autostartmodel.cpp" line="179"/>
+        <location line="179" filename="../autostartmodel.cpp"/>
         <source>LXQt Autostart</source>
-        <translation type="unfinished"></translation>
+        <translation>Αυτόματη εκκίνηση LXQt</translation>
     </message>
     <message>
-        <location filename="../autostartmodel.cpp" line="197"/>
+        <location line="197" filename="../autostartmodel.cpp"/>
         <source>Location: %1</source>
         <translation>Τοποθεσία: %1</translation>
     </message>
     <message>
-        <location filename="../autostartmodel.cpp" line="199"/>
+        <location line="199" filename="../autostartmodel.cpp"/>
         <source>Overrides: %1</source>
         <translation>Αντικατάσταση: %1</translation>
     </message>
@@ -64,65 +64,65 @@
 <context>
     <name>AutoStartPage</name>
     <message>
-        <location filename="../autostartpage.ui" line="17"/>
+        <location line="17" filename="../autostartpage.ui"/>
         <source>Delete</source>
         <translation>Διαγραφή</translation>
     </message>
     <message>
-        <location filename="../autostartpage.ui" line="24"/>
+        <location line="24" filename="../autostartpage.ui"/>
         <source>Add</source>
         <translation>Προσθήκη</translation>
     </message>
     <message>
-        <location filename="../autostartpage.ui" line="31"/>
+        <location line="31" filename="../autostartpage.ui"/>
         <source>Edit</source>
         <translation>Επεξεργασία</translation>
     </message>
     <message>
-        <location filename="../autostartpage.ui" line="44"/>
+        <location line="44" filename="../autostartpage.ui"/>
         <source>Application Autostart</source>
         <translation>Αυτόματη εκκίνηση εφαρμογής</translation>
     </message>
     <message>
-        <location filename="../autostartpage.cpp" line="82"/>
+        <location line="82" filename="../autostartpage.cpp"/>
         <source>Error</source>
         <translation>Σφάλμα</translation>
     </message>
     <message>
-        <location filename="../autostartpage.cpp" line="82"/>
-        <source>File &apos;%1&apos; already exists!</source>
-        <translation>Το αρχείο &quot;%1&quot; υπάρχει ήδη!</translation>
+        <location line="82" filename="../autostartpage.cpp"/>
+        <source>File '%1' already exists!</source>
+        <translation>Το αρχείο "%1" υπάρχει ήδη!</translation>
     </message>
 </context>
 <context>
     <name>BasicSettings</name>
     <message>
-        <location filename="../basicsettings.ui" line="30"/>
+        <location line="30" filename="../basicsettings.ui"/>
         <source>Window Manager</source>
         <translation>Διαχειριστής παραθύρων</translation>
     </message>
     <message>
-        <location filename="../basicsettings.ui" line="49"/>
+        <location line="49" filename="../basicsettings.ui"/>
         <source>Search...</source>
         <translation>Αναζήτηση...</translation>
     </message>
     <message>
-        <location filename="../basicsettings.ui" line="59"/>
+        <location line="59" filename="../basicsettings.ui"/>
         <source>LXQt Modules</source>
-        <translation type="unfinished"></translation>
+        <translation>Αρθρώματα LXQt</translation>
     </message>
     <message>
-        <location filename="../basicsettings.ui" line="111"/>
+        <location line="111" filename="../basicsettings.ui"/>
         <source>Leave Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Εγκατάλειψη της συνεδρίας</translation>
     </message>
     <message>
-        <location filename="../basicsettings.ui" line="117"/>
+        <location line="117" filename="../basicsettings.ui"/>
         <source>Ask confirmation to leave session</source>
-        <translation type="unfinished"></translation>
+        <translation>Ερώτηση επιβεβαίωσης κατά την εγκατάλειψη της συνεδρίας</translation>
     </message>
     <message>
-        <location filename="../basicsettings.ui" line="23"/>
+        <location line="23" filename="../basicsettings.ui"/>
         <source>Basic Settings</source>
         <translation>Βασικές ρυθμίσεις</translation>
     </message>
@@ -131,17 +131,17 @@
         <translation type="vanished">Αρθρώματα LxQt</translation>
     </message>
     <message>
-        <location filename="../basicsettings.ui" line="65"/>
+        <location line="65" filename="../basicsettings.ui"/>
         <source>Start</source>
         <translation>Εκκίνηση</translation>
     </message>
     <message>
-        <location filename="../basicsettings.ui" line="72"/>
+        <location line="72" filename="../basicsettings.ui"/>
         <source>Stop</source>
         <translation>Διακοπή</translation>
     </message>
     <message>
-        <location filename="../basicsettings.cpp" line="84"/>
+        <location line="84" filename="../basicsettings.cpp"/>
         <source>Select a window manager</source>
         <translation>Επιλογή διαχειριστή παραθύρων</translation>
     </message>
@@ -149,12 +149,12 @@
 <context>
     <name>DefaultApps</name>
     <message>
-        <location filename="../defaultappspage.cpp" line="72"/>
+        <location line="72" filename="../defaultappspage.cpp"/>
         <source>Select a terminal emulator</source>
         <translation>Επιλογή εξομοιωτή τερματικού</translation>
     </message>
     <message>
-        <location filename="../defaultappspage.cpp" line="77"/>
+        <location line="77" filename="../defaultappspage.cpp"/>
         <source>Select a web browser</source>
         <translation>Επιλογή περιηγητή ιστού</translation>
     </message>
@@ -162,23 +162,23 @@
 <context>
     <name>DefaultAppsPage</name>
     <message>
-        <location filename="../defaultappspage.ui" line="23"/>
+        <location line="23" filename="../defaultappspage.ui"/>
         <source>Default Applications</source>
         <translation>Προεπιλεγμένες εφαρμογές</translation>
     </message>
     <message>
-        <location filename="../defaultappspage.ui" line="32"/>
+        <location line="32" filename="../defaultappspage.ui"/>
         <source>Terminal Emulation:</source>
         <translation>Εξομοιωτής τερματικού:</translation>
     </message>
     <message>
-        <location filename="../defaultappspage.ui" line="52"/>
-        <location filename="../defaultappspage.ui" line="79"/>
+        <location line="52" filename="../defaultappspage.ui"/>
+        <location line="79" filename="../defaultappspage.ui"/>
         <source>Search...</source>
         <translation>Αναζήτηση...</translation>
     </message>
     <message>
-        <location filename="../defaultappspage.ui" line="59"/>
+        <location line="59" filename="../defaultappspage.ui"/>
         <source>Web Browser:</source>
         <translation>Περιηγητής ιστού:</translation>
     </message>
@@ -186,27 +186,27 @@
 <context>
     <name>EnvironmentPage</name>
     <message>
-        <location filename="../environmentpage.ui" line="30"/>
+        <location line="30" filename="../environmentpage.ui"/>
         <source>Add</source>
         <translation>Προσθήκη</translation>
     </message>
     <message>
-        <location filename="../environmentpage.ui" line="37"/>
+        <location line="37" filename="../environmentpage.ui"/>
         <source>Delete</source>
         <translation>Διαγραφή</translation>
     </message>
     <message>
-        <location filename="../environmentpage.ui" line="63"/>
+        <location line="63" filename="../environmentpage.ui"/>
         <source>Environment Variables (Advanced)</source>
         <translation>Μεταβλητές περιβάλλοντος (για προχωρημένους)</translation>
     </message>
     <message>
-        <location filename="../environmentpage.ui" line="74"/>
+        <location line="74" filename="../environmentpage.ui"/>
         <source>Variable Name</source>
         <translation>Όνομα μεταβλητής</translation>
     </message>
     <message>
-        <location filename="../environmentpage.ui" line="79"/>
+        <location line="79" filename="../environmentpage.ui"/>
         <source>Value</source>
         <translation>Τιμή</translation>
     </message>
@@ -214,7 +214,7 @@
 <context>
     <name>ModuleModel</name>
     <message>
-        <location filename="../modulemodel.cpp" line="85"/>
+        <location line="85" filename="../modulemodel.cpp"/>
         <source>Running</source>
         <translation>Εκτελείται</translation>
     </message>
@@ -226,37 +226,37 @@
         <translation type="vanished">Διαμόρφωση συνεδρίας LxQt</translation>
     </message>
     <message>
-        <location filename="../sessionconfigwindow.cpp" line="43"/>
+        <location line="43" filename="../sessionconfigwindow.cpp"/>
         <source>LXQt Session Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ρυθμίσεις συνεδρίας LXQt</translation>
     </message>
     <message>
-        <location filename="../sessionconfigwindow.cpp" line="46"/>
+        <location line="46" filename="../sessionconfigwindow.cpp"/>
         <source>Basic Settings</source>
         <translation>Βασικές ρυθμίσεις</translation>
     </message>
     <message>
-        <location filename="../sessionconfigwindow.cpp" line="52"/>
+        <location line="52" filename="../sessionconfigwindow.cpp"/>
         <source>Default Applications</source>
         <translation>Προεπιλεγμένες εφαρμογές</translation>
     </message>
     <message>
-        <location filename="../sessionconfigwindow.cpp" line="55"/>
+        <location line="55" filename="../sessionconfigwindow.cpp"/>
         <source>Autostart</source>
         <translation>Αυτόματη εκκίνηση</translation>
     </message>
     <message>
-        <location filename="../sessionconfigwindow.cpp" line="60"/>
+        <location line="60" filename="../sessionconfigwindow.cpp"/>
         <source>Environment (Advanced)</source>
         <translation>Περιβάλλον (για προχωρημένους)</translation>
     </message>
     <message>
-        <location filename="../sessionconfigwindow.cpp" line="84"/>
+        <location line="84" filename="../sessionconfigwindow.cpp"/>
         <source>Session Restart Required</source>
         <translation>Απαιτείται επανεκκίνηση συνεδρίας</translation>
     </message>
     <message>
-        <location filename="../sessionconfigwindow.cpp" line="85"/>
+        <location line="85" filename="../sessionconfigwindow.cpp"/>
         <source>You need to restart desktop session (lxqt-session) to reload settings. Use logout from the main menu.</source>
         <translation>Πρέπει να επανεκκινήσετε τη συνεδρία lxqt για να φορτώσετε τις ρυθμίσεις. Χρησιμοποιήστε την αποσύνδεση από το κυρίως μενού.</translation>
     </message>
--- lxqt-session/translations/lxqt-session_el_GR.ts
+++ lxqt-session/translations/lxqt-session_el_GR.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el_GR">
 <context>
@@ -8,62 +8,62 @@
         <translation type="vanished">Αναφορά κατάρρευσης συνεδρίας LxQt</translation>
     </message>
     <message>
-        <source>Application &apos;%1&apos; crashed too many times. Its autorestart has been disabled for current session.</source>
-        <translation type="vanished">Η εφαρμογή &quot;%1&quot; κατέρρευσε πολλές φορές. Η αυτόματη εκκίνησή της έχει απενεργοποιηθεί για την τρέχουσα συνεδρία.</translation>
+        <source>Application '%1' crashed too many times. Its autorestart has been disabled for current session.</source>
+        <translation type="vanished">Η εφαρμογή "%1" κατέρρευσε πολλές φορές. Η αυτόματη εκκίνησή της έχει απενεργοποιηθεί για την τρέχουσα συνεδρία.</translation>
     </message>
     <message>
-        <location filename="../src/lxqtmodman.cpp" line="289"/>
+        <location line="289" filename="../src/lxqtmodman.cpp"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Αναφορά κατάρρευσης</translation>
     </message>
     <message>
-        <location filename="../src/lxqtmodman.cpp" line="290"/>
-        <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <location line="290" filename="../src/lxqtmodman.cpp"/>
+        <source>&lt;b>%1&lt;/b> crashed too many times. Its autorestart has been disabled until next login.</source>
+        <translation>Το &lt;b>%1&lt;/b> κατέρρευσε πάρα πολλές φορές. Η επανεκκίνησή του απενεργοποιήθηκε μέχρι την επόμενη σύνδεση.</translation>
     </message>
 </context>
 <context>
     <name>WmSelectDialog</name>
     <message>
         <source>Welcome to LXQt</source>
-        <translation type="vanished">Καλως ήλθατε στο LXQt</translation>
+        <translation>Καλώς ήλθατε στο LXQt</translation>
     </message>
     <message>
-        <source>&lt;b&gt;Welcome to LXQt&lt;/b&gt;
-&lt;p&gt;
+        <source>&lt;b>Welcome to LXQt&lt;/b>
+&lt;p>
 Before starting to use the LXQt, you might want to select the Windows Manager:</source>
-        <translation type="vanished">&lt;b&gt;Καλως ήλθατε στο LXQt&lt;/b&gt;
-&lt;p&gt;
-Πριν ξεκινήσετε να χρησιμοποιείτε το LXQt, ίσως θέλετε να επιλέξετε ένα διαχειριστή παραθύρων:</translation>
+        <translation>&lt;b>Καλώς ήλθατε στο LXQt&lt;/b>
+&lt;p>
+Πριν ξεκινήσετε να χρησιμοποιείτε το LXQt, ίσως θέλετε να επιλέξετε τον διαχειριστή παραθύρων:</translation>
     </message>
     <message>
         <source>You can change the Window Manager later at any time via LxQt Session Configurator.</source>
         <translation type="vanished">Μπορείτε να αλλάξετε τον διαχειριστή παραθύρων αργότερα μέσω της διαμόρφωσης συνεδρίας LxQt.</translation>
     </message>
     <message>
-        <location filename="../src/wmselectdialog.cpp" line="62"/>
+        <location line="62" filename="../src/wmselectdialog.cpp"/>
         <source>Other ...</source>
         <translation>Άλλο...</translation>
     </message>
     <message>
-        <location filename="../src/wmselectdialog.cpp" line="63"/>
+        <location line="63" filename="../src/wmselectdialog.cpp"/>
         <source>Choose your favorite one.</source>
         <translation>Επιλέξτε το αγαπημένο σας.</translation>
     </message>
     <message>
-        <location filename="../src/wmselectdialog.ui" line="14"/>
+        <location line="14" filename="../src/wmselectdialog.ui"/>
         <source>Welcome to LXQt</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/wmselectdialog.ui" line="20"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <location line="20" filename="../src/wmselectdialog.ui"/>
+        <source>&lt;html>&lt;head/>&lt;body>&lt;p>&lt;span style=" font-weight:600;">Welcome to LXQt&lt;/span>&lt;/p>&lt;p>Please select your default Window Manager.&lt;/p>&lt;/body>&lt;/html></source>
+        <translation>&lt;html>&lt;head/>&lt;body>&lt;p>&lt;span style=" font-weight:600;">Καλώς ήλθατε στο LXQt&lt;/span>&lt;/p>&lt;p>Παρακαλώ επιλέξτε τον εξ ορισμού διαχειριστή παραθύρων.&lt;/p>&lt;/body>&lt;/html></translation>
     </message>
     <message>
-        <location filename="../src/wmselectdialog.ui" line="71"/>
-        <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <location line="71" filename="../src/wmselectdialog.ui"/>
+        <source>You will be able to change this at any time through Preferences -> Session Settings -> Basic Settings.</source>
+        <translation>Μπορείτε να αλλάξετε αυτήν τη ρύθμιση από τις Προτιμήσεις -> Διαμόρφωση συνεδρίας -> Βασικές ρυθμίσεις.</translation>
     </message>
 </context>
 </TS>
