915df36178d07b2b539ecefcdbcb8e044684a76e
Palo Kisa <palo.kisa@gmail.com>
2016-03-30 03:33:30 +0200

    fixup...centering & spacing

--- lxqt-leave/main.cpp
+++ lxqt-leave/main.cpp
@@ -102,11 +102,10 @@ int main(int argc, char *argv[])
 
     LeaveDialog dialog;
     a.setActivationWindow(&dialog);
-    dialog.setMinimumSize(dialog.sizeHint());
+    dialog.setFixedSize(dialog.sizeHint());
     dialog.setGeometry(QStyle::alignedRect(Qt::LeftToRight,
                 Qt::AlignCenter,
-                dialog.size(),
+                dialog.sizeHint(),
                 qApp->desktop()->screenGeometry(QCursor::pos())));
-    dialog.setMaximumSize(dialog.size());
     return dialog.exec();
 }
