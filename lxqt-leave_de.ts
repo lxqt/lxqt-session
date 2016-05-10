7cb1e65a017187ef38e00abfbeceef8efb07fe10
Alf Gaida <agaida@siduction.org>
2016-05-10 20:03:15 +0200

    Some renaming
    Renamed lxqt-config-session/translations to lxqt-session/lxqt-config-session
    Renamed lxqt-leave/translations to lxqt-session/lxqt-leave
    REnamed lxqt-session/translations to  lxqt-session/lxqt-session

---
+++ lxqt-leave_de.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Verlassen</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="29"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Was soll der Computer tun?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="45"/>
+        <source>Logout</source>
+        <translation>Abmelden</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="57"/>
+        <source>Reboot</source>
+        <translation>Neustart</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="69"/>
+        <source>Shutdown</source>
+        <translation>Herunterfahren</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="81"/>
+        <source>Suspend</source>
+        <translation>Bereitschaft</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="93"/>
+        <source>Lock screen</source>
+        <translation>Bildschirm sperren</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="105"/>
+        <source>Hibernate</source>
+        <translation>Tiefschlaf</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="136"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="51"/>
+        <source>Logout.</source>
+        <translation>Abmelden.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="54"/>
+        <source>Lockscreen.</source>
+        <translation>Sperren.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="57"/>
+        <source>Suspend.</source>
+        <translation>Bereitschaft.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Hibernate.</source>
+        <translation>Tiefschlaf.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="63"/>
+        <source>Shutdown.</source>
+        <translation>Herunterfahren.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="66"/>
+        <source>Reboot.</source>
+        <translation>Neustart.</translation>
+    </message>
+</context>
+</TS>
---
+++ lxqt-leave_el.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Έξοδος</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="29"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Τι επιθυμείτε να πραγματοποιήσει ο υπολογιστής;&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="45"/>
+        <source>Logout</source>
+        <translation>Αποσύνδεση</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="57"/>
+        <source>Reboot</source>
+        <translation>Επανεκκίνηση</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="69"/>
+        <source>Shutdown</source>
+        <translation>Τερματισμός</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="81"/>
+        <source>Suspend</source>
+        <translation>Αναστολή</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="93"/>
+        <source>Lock screen</source>
+        <translation>Κλείδωμα οθόνης</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="105"/>
+        <source>Hibernate</source>
+        <translation>Νάρκη</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="136"/>
+        <source>Cancel</source>
+        <translation>Ακύρωση</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="51"/>
+        <source>Logout.</source>
+        <translation>Αποσύνδεση.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="54"/>
+        <source>Lockscreen.</source>
+        <translation>Κλείδωμα οθόνης.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="57"/>
+        <source>Suspend.</source>
+        <translation>Αναστολή.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Hibernate.</source>
+        <translation>Νάρκη.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="63"/>
+        <source>Shutdown.</source>
+        <translation>Τερματισμός.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="66"/>
+        <source>Reboot.</source>
+        <translation>Επανεκκίνηση.</translation>
+    </message>
+</context>
+</TS>
