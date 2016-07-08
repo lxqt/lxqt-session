9f290c4022dac42da3538521013113ba78341df6
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

---
+++ lxqt-leave_ar.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../../../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>اترك</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="59"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;ما الذي تريد من الحاسوب فعله؟&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="92"/>
+        <source>Logout</source>
+        <translation>اخرج</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="206"/>
+        <source>Reboot</source>
+        <translation>أعد الإقلاع</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="186"/>
+        <source>Shutdown</source>
+        <translation>أطفئ</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="139"/>
+        <source>Suspend</source>
+        <translation>علّق</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="112"/>
+        <source>Lock screen</source>
+        <translation>اقفل الشّاشة</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="159"/>
+        <source>Hibernate</source>
+        <translation>أسبت</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="42"/>
+        <source>Cancel</source>
+        <translation>ألغِ</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../../main.cpp" line="51"/>
+        <source>Logout.</source>
+        <translation>اخرج.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="54"/>
+        <source>Lockscreen.</source>
+        <translation>اقفل الشّاشة.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="57"/>
+        <source>Suspend.</source>
+        <translation>علّق.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="60"/>
+        <source>Hibernate.</source>
+        <translation>أسبت.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="63"/>
+        <source>Shutdown.</source>
+        <translation>أطفئ.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="66"/>
+        <source>Reboot.</source>
+        <translation>أعد الإقلاع.</translation>
+    </message>
+</context>
+</TS>
