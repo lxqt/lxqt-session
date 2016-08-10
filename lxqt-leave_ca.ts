ccaf4a666f3888d3ac52b85df40ce835d48a06cf
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-10 16:58:27 +0200

    Add lxqt-leave_ca.ts (Catalan)

---
+++ lxqt-leave_ca.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../../../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Sortida</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="59"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Què voleu que faci l&apos;ordinador?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="92"/>
+        <source>Logout</source>
+        <translation>Tanca la sessió</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="206"/>
+        <source>Reboot</source>
+        <translation>Reinicia</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="186"/>
+        <source>Shutdown</source>
+        <translation>Apaga</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="139"/>
+        <source>Suspend</source>
+        <translation>Suspèn</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="112"/>
+        <source>Lock screen</source>
+        <translation>Bloqueja la pantalla</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="159"/>
+        <source>Hibernate</source>
+        <translation>Hiberna</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="42"/>
+        <source>Cancel</source>
+        <translation>Cancel·la</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../../main.cpp" line="51"/>
+        <source>Logout.</source>
+        <translation>Tanca la sessió.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="54"/>
+        <source>Lockscreen.</source>
+        <translation>Bloqueja la pantalla.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="57"/>
+        <source>Suspend.</source>
+        <translation>Suspèn.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="60"/>
+        <source>Hibernate.</source>
+        <translation>Hiberna.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="63"/>
+        <source>Shutdown.</source>
+        <translation>Apaga.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="66"/>
+        <source>Reboot.</source>
+        <translation>Reinicia.</translation>
+    </message>
+</context>
+</TS>
