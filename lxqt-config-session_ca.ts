c1bcd7588a4c5a7e75333672320838d32f51276a
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-10 16:41:49 +0200

    Translate lxqt-config-session_ca.ts (Catalan)

--- lxqt-config-session_ca.ts
+++ lxqt-config-session_ca.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../../../autostartedit.ui" line="14"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Edició</translation>
     </message>
     <message>
         <location filename="../../../autostartedit.ui" line="20"/>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom:</translation>
     </message>
     <message>
         <location filename="../../../autostartedit.ui" line="27"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Cerca...</translation>
     </message>
     <message>
         <location filename="../../../autostartedit.ui" line="47"/>
         <source>Command:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ordre:</translation>
     </message>
     <message>
         <location filename="../../../autostartedit.ui" line="57"/>
         <source>Wait for system tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Espera la safata del sistema</translation>
     </message>
     <message>
         <location filename="../../../autostartedit.cpp" line="59"/>
         <source>Select Application</source>
-        <translation type="unfinished"></translation>
+        <translation>Selecció de l&apos;aplicació</translation>
     </message>
 </context>
 <context>
@@ -39,22 +39,22 @@
     <message>
         <location filename="../../../autostartmodel.cpp" line="191"/>
         <source>Global Autostart</source>
-        <translation type="unfinished"></translation>
+        <translation>Inicialització automàtica global</translation>
     </message>
     <message>
         <location filename="../../../autostartmodel.cpp" line="193"/>
         <source>LXQt Autostart</source>
-        <translation type="unfinished"></translation>
+        <translation>Inicialització automàtica de LXQt</translation>
     </message>
     <message>
         <location filename="../../../autostartmodel.cpp" line="211"/>
         <source>Location: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ubicació: %1</translation>
     </message>
     <message>
         <location filename="../../../autostartmodel.cpp" line="213"/>
         <source>Overrides: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Anul·la: %1</translation>
     </message>
 </context>
 <context>
@@ -62,80 +62,80 @@
     <message>
         <location filename="../../../autostartpage.ui" line="17"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Suprimeix</translation>
     </message>
     <message>
         <location filename="../../../autostartpage.ui" line="24"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Afegeix</translation>
     </message>
     <message>
         <location filename="../../../autostartpage.ui" line="31"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Edita</translation>
     </message>
     <message>
         <location filename="../../../autostartpage.ui" line="44"/>
         <source>Application Autostart</source>
-        <translation type="unfinished"></translation>
+        <translation>Inicialització automàtica d&apos;aplicacions</translation>
     </message>
     <message>
         <location filename="../../../autostartpage.cpp" line="141"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Error</translation>
     </message>
     <message>
         <location filename="../../../autostartpage.cpp" line="141"/>
         <source>File &apos;%1&apos; already exists!</source>
-        <translation type="unfinished"></translation>
+        <translation>Ja existeix el fitxer «%1»!</translation>
     </message>
 </context>
 <context>
     <name>BasicSettings</name>
     <message>
+        <location filename="../../../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation>Ajusts bàsics</translation>
+    </message>
+    <message>
         <location filename="../../../basicsettings.ui" line="30"/>
         <source>Window Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Gestor de finestres</translation>
     </message>
     <message>
         <location filename="../../../basicsettings.ui" line="49"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Cerca...</translation>
     </message>
     <message>
         <location filename="../../../basicsettings.ui" line="59"/>
         <source>LXQt Modules</source>
-        <translation type="unfinished"></translation>
+        <translation>Mòduls de LXQt</translation>
     </message>
     <message>
         <location filename="../../../basicsettings.ui" line="65"/>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>Inicia</translation>
     </message>
     <message>
         <location filename="../../../basicsettings.ui" line="72"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>Atura</translation>
     </message>
     <message>
         <location filename="../../../basicsettings.ui" line="111"/>
         <source>Leave Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Sortida de la sessió</translation>
     </message>
     <message>
         <location filename="../../../basicsettings.ui" line="117"/>
         <source>Ask for confirmation to leave session</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../basicsettings.ui" line="23"/>
-        <source>Basic Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Demana la confirmació per sortir de la sessió</translation>
     </message>
     <message>
         <location filename="../../../basicsettings.cpp" line="137"/>
         <source>Select a window manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccioneu un gestor de finestres</translation>
     </message>
 </context>
 <context>
@@ -143,12 +143,12 @@
     <message>
         <location filename="../../../defaultappspage.cpp" line="72"/>
         <source>Select a terminal emulator</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccioneu un emulador de terminal</translation>
     </message>
     <message>
         <location filename="../../../defaultappspage.cpp" line="77"/>
         <source>Select a web browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccioneu un navegador web</translation>
     </message>
 </context>
 <context>
@@ -156,23 +156,23 @@
     <message>
         <location filename="../../../defaultappspage.ui" line="23"/>
         <source>Default Applications</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplicacions predeterminades</translation>
     </message>
     <message>
         <location filename="../../../defaultappspage.ui" line="32"/>
         <source>Terminal Emulation:</source>
-        <translation type="unfinished"></translation>
+        <translation>Emulació de terminal:</translation>
     </message>
     <message>
         <location filename="../../../defaultappspage.ui" line="52"/>
         <location filename="../../../defaultappspage.ui" line="79"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Cerca...</translation>
     </message>
     <message>
         <location filename="../../../defaultappspage.ui" line="59"/>
         <source>Web Browser:</source>
-        <translation type="unfinished"></translation>
+        <translation>Navegador web:</translation>
     </message>
 </context>
 <context>
@@ -180,27 +180,27 @@
     <message>
         <location filename="../../../environmentpage.ui" line="17"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Afegeix</translation>
     </message>
     <message>
         <location filename="../../../environmentpage.ui" line="24"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Suprimeix</translation>
     </message>
     <message>
         <location filename="../../../environmentpage.ui" line="50"/>
         <source>Environment Variables (Advanced)</source>
-        <translation type="unfinished"></translation>
+        <translation>Variables d&apos;entorn (avançat)</translation>
     </message>
     <message>
         <location filename="../../../environmentpage.ui" line="61"/>
         <source>Variable Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom de la variable</translation>
     </message>
     <message>
         <location filename="../../../environmentpage.ui" line="66"/>
         <source>Value</source>
-        <translation type="unfinished"></translation>
+        <translation>Valor</translation>
     </message>
 </context>
 <context>
@@ -208,7 +208,7 @@
     <message>
         <location filename="../../../modulemodel.cpp" line="86"/>
         <source>Running</source>
-        <translation type="unfinished"></translation>
+        <translation>En execució</translation>
     </message>
 </context>
 <context>
@@ -216,42 +216,42 @@
     <message>
         <location filename="../../../sessionconfigwindow.cpp" line="44"/>
         <source>LXQt Session Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajusts de la sessió LXQt</translation>
     </message>
     <message>
         <location filename="../../../sessionconfigwindow.cpp" line="47"/>
         <source>Basic Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajusts bàsics</translation>
     </message>
     <message>
         <location filename="../../../sessionconfigwindow.cpp" line="53"/>
         <source>Default Applications</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplicacions predeterminades</translation>
     </message>
     <message>
         <location filename="../../../sessionconfigwindow.cpp" line="56"/>
         <source>User Directories</source>
-        <translation type="unfinished"></translation>
+        <translation>Directoris dels usuaris</translation>
     </message>
     <message>
         <location filename="../../../sessionconfigwindow.cpp" line="62"/>
         <source>Autostart</source>
-        <translation type="unfinished"></translation>
+        <translation>Inicialització automàtica</translation>
     </message>
     <message>
         <location filename="../../../sessionconfigwindow.cpp" line="68"/>
         <source>Environment (Advanced)</source>
-        <translation type="unfinished"></translation>
+        <translation>Entorn (avançat)</translation>
     </message>
     <message>
         <location filename="../../../sessionconfigwindow.cpp" line="94"/>
         <source>Session Restart Required</source>
-        <translation type="unfinished"></translation>
+        <translation>Requeriment de reinici de sessió</translation>
     </message>
     <message>
         <location filename="../../../sessionconfigwindow.cpp" line="95"/>
         <source>Some settings will not take effect until the next log in.</source>
-        <translation type="unfinished"></translation>
+        <translation>Alguns ajusts no tindran efecte fins al següent inici de sessió.</translation>
     </message>
 </context>
 <context>
@@ -259,22 +259,22 @@
     <message>
         <location filename="../../../userlocationspage.cpp" line="110"/>
         <source>Locations for Personal Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Ubicacions per als fitxers personals</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="179"/>
         <source>LXQt Session Settings - User Directories</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajusts de la sessió LXQt - Directoris dels usuaris</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="180"/>
         <source>An error ocurred while applying the settings for the %1 location</source>
-        <translation type="unfinished"></translation>
+        <translation>S&apos;ha produït un error mentre s&apos;aplicaven els ajusts per a la ubicació %1</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="196"/>
         <source>Choose Location</source>
-        <translation type="unfinished"></translation>
+        <translation>Tria una ubicació</translation>
     </message>
 </context>
 <context>
@@ -282,82 +282,82 @@
     <message>
         <location filename="../../../userlocationspage.cpp" line="60"/>
         <source>Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Escriptori</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="61"/>
         <source>Downloads</source>
-        <translation type="unfinished"></translation>
+        <translation>Baixades</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="62"/>
         <source>Templates</source>
-        <translation type="unfinished"></translation>
+        <translation>Plantilles</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="63"/>
         <source>Public Share</source>
-        <translation type="unfinished"></translation>
+        <translation>Compartició pública</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="64"/>
         <source>Documents</source>
-        <translation type="unfinished"></translation>
+        <translation>Documents</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="65"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Música</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="66"/>
         <source>Pictures</source>
-        <translation type="unfinished"></translation>
+        <translation>Imatges</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="67"/>
         <source>Videos</source>
-        <translation type="unfinished"></translation>
+        <translation>Vídeos</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="70"/>
         <source>Contains all the files which you see on your desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Conté tots els fitxers que veieu al vostre escriptori</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="71"/>
         <source>Default folder to save your downloaded files</source>
-        <translation type="unfinished"></translation>
+        <translation>Carpeta predeterminada per desar els vostres fitxers baixats</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="72"/>
         <source>Default folder to load or save templates from or to</source>
-        <translation type="unfinished"></translation>
+        <translation>Carpeta predeterminada per carregar o desar-hi plantilles</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="73"/>
         <source>Default folder to publicly share your files</source>
-        <translation type="unfinished"></translation>
+        <translation>Carpeta predeterminada per a la compartició pública dels vostres fitxers</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="74"/>
         <source>Default folder to load or save documents from or to</source>
-        <translation type="unfinished"></translation>
+        <translation>Carpeta predeterminada per carregar o desar-hi documents</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="75"/>
         <source>Default foldet to load or save music from or to</source>
-        <translation type="unfinished"></translation>
+        <translation>Carpeta predeterminada per carregar o desar-hi música</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="76"/>
         <source>Default folder to load or save pictures from or to</source>
-        <translation type="unfinished"></translation>
+        <translation>Carpeta predeterminada per carregar o desar-hi imatges</translation>
     </message>
     <message>
         <location filename="../../../userlocationspage.cpp" line="77"/>
         <source>Default folder to load or save videos from or to</source>
-        <translation type="unfinished"></translation>
+        <translation>Carpeta predeterminada per carregar o desar-hi vídeos</translation>
     </message>
 </context>
 </TS>
