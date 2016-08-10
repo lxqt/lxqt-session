83238b9220ad57569426acaa68c166084ec7c3c0
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-10 17:12:30 +0200

    Translate lxqt-session_ca.ts (Catalan)

--- lxqt-session_ca.ts
+++ lxqt-session_ca.ts
@@ -6,40 +6,40 @@
     <message>
         <location filename="../../../src/lxqtmodman.cpp" line="302"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Informació de col·lisió</translation>
     </message>
     <message>
         <location filename="../../../src/lxqtmodman.cpp" line="303"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; s&apos;ha estavellat moltes vegades. Els seus reinicis automàtics han estat inhabilitats fins al següent inici de sessió.</translation>
     </message>
 </context>
 <context>
     <name>WmSelectDialog</name>
     <message>
-        <location filename="../../../src/wmselectdialog.cpp" line="62"/>
-        <source>Other ...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../src/wmselectdialog.cpp" line="63"/>
-        <source>Choose your favorite one.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../../../src/wmselectdialog.ui" line="14"/>
         <source>Welcome to LXQt</source>
-        <translation type="unfinished"></translation>
+        <translation>Benvingut a LXQt</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Benvingut a LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Seleccioneu el vostre gestor de finestres preferit.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.ui" line="71"/>
         <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Podreu canviar-ho en qualsevol moment a través de Preferències -&gt; Ajusts de la sessió -&gt; Ajusts bàsics.</translation>
+    </message>
+    <message>
+        <location filename="../../../src/wmselectdialog.cpp" line="62"/>
+        <source>Other ...</source>
+        <translation>Altres...</translation>
+    </message>
+    <message>
+        <location filename="../../../src/wmselectdialog.cpp" line="63"/>
+        <source>Choose your favorite one.</source>
+        <translation>Trieu el vostre preferit.</translation>
     </message>
 </context>
 </TS>
