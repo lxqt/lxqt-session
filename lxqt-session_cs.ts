64a696f3fbdd5a13998fdec139227bd3f2cb6f7c
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

--- lxqt-session_cs.ts
+++ lxqt-session_cs.ts
@@ -5,21 +5,21 @@
     <name>LXQtModuleManager</name>
     <message>
         <source>LXQt Session Crash Report</source>
-        <translation type="vanished">Zpráva o pádu sezení LXQtu</translation>
+        <translation>Zpráva o pádu sezení LXQtu</translation>
     </message>
     <message>
         <source>Application &apos;%1&apos; crashed too many times. Its autorestart has been disabled for current session.</source>
-        <translation type="vanished">Program &apos;%1&apos; spadl příliš mnohokrát. Jeho automatické spuštění bylo pro nynější sezení zakázáno.</translation>
+        <translation>Program &apos;%1&apos; spadl příliš mnohokrát. Jeho automatické spuštění bylo pro nynější sezení zakázáno.</translation>
     </message>
     <message>
         <location filename="../../../src/lxqtmodman.cpp" line="302"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Zpráva o pádu</translation>
     </message>
     <message>
         <location filename="../../../src/lxqtmodman.cpp" line="303"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; spadl příliš mockrát. Automatické znovuspuštění bylo zakázáno až do příštího přihlášení.</translation>
     </message>
 </context>
 <context>
@@ -33,13 +33,13 @@
         <source>&lt;b&gt;Welcome to LXQt&lt;/b&gt;
 &lt;p&gt;
 Before starting to use the LXQt, you might want to select the Windows Manager:</source>
-        <translation type="vanished">&lt;b&gt;Vítejte v LXQt-Qt&lt;/b&gt;
+        <translation>&lt;b&gt;Vítejte v LXQt-Qt&lt;/b&gt;
 &lt;p&gt;
 Před započetím s prací byste mohl chtít vybrat si správce oken:</translation>
     </message>
     <message>
         <source>You can change the Window Manager later at any time via LXQt Session Configurator.</source>
-        <translation type="vanished">Správce oken můžete později změnit v nastavení sezení pro LXQt.</translation>
+        <translation>Správce oken můžete později změnit v nastavení sezení pro LXQt.</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.cpp" line="62"/>
@@ -54,12 +54,12 @@ Před započetím s prací byste mohl chtít vybrat si správce oken:</translati
     <message>
         <location filename="../../../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Vítejte v LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Vyberte, prosím, výchozího správce oken.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.ui" line="71"/>
         <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Toto budete moci kdykoli změnit přes Nastavení → Nastavení sezení → Základní nastavení.</translation>
     </message>
 </context>
 </TS>
