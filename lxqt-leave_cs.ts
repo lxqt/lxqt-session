32b1c8e3d33483ec4d91b74605e02ea834d15bd5
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

---
+++ lxqt-leave_cs.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../../../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Odejít</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="59"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Co po počítači chcete, aby udělal?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="92"/>
+        <source>Logout</source>
+        <translation>Odhlásit se</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="206"/>
+        <source>Reboot</source>
+        <translation>Restartovat</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="186"/>
+        <source>Shutdown</source>
+        <translation>Vypnout</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="139"/>
+        <source>Suspend</source>
+        <translation>Uspat</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="112"/>
+        <source>Lock screen</source>
+        <translation>Uzamknout</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="159"/>
+        <source>Hibernate</source>
+        <translation>Přezimovat</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="42"/>
+        <source>Cancel</source>
+        <translation>Zrušit</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../../main.cpp" line="51"/>
+        <source>Logout.</source>
+        <translation>Odhlásit se.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="54"/>
+        <source>Lockscreen.</source>
+        <translation>Uzamknout obrazovku.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="57"/>
+        <source>Suspend.</source>
+        <translation>Uspat.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="60"/>
+        <source>Hibernate.</source>
+        <translation>Přezimovat (hibernace).</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="63"/>
+        <source>Shutdown.</source>
+        <translation>Vypnout.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="66"/>
+        <source>Reboot.</source>
+        <translation>Restartovat.</translation>
+    </message>
+</context>
+</TS>
