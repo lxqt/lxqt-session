9246b8cd6a1f923aea26d4a8cdf5ce115cf0f9c8
Pjotr123 <pjotrvertaalt@gmail.com>
2016-10-16 15:52:57 +0200

    Update lxqt-session_nl.ts
    Complete Dutch translation

--- lxqt-session_nl.ts
+++ lxqt-session_nl.ts
@@ -4,22 +4,14 @@
 <context>
     <name>LXQtModuleManager</name>
     <message>
-        <source>LXQt Session Crash Report</source>
-        <translation type="vanished">LXQt Sessie Fout Rapport</translation>
-    </message>
-    <message>
-        <source>Application &apos;%1&apos; crashed too many times. Its autorestart has been disabled for current session.</source>
-        <translation type="vanished">Programma &apos;%1&apos; is te vaak gecrashed. De autorestart functie is uitgeschakeld voor deze sessie.</translation>
-    </message>
-    <message>
         <location filename="../../../src/lxqtmodman.cpp" line="302"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Vastlooprapport</translation>
     </message>
     <message>
         <location filename="../../../src/lxqtmodman.cpp" line="303"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; is te vaak vastgelopen. Zijn automatische herstart is uitgeschakeld tot de volgende aanmelding.</translation>
     </message>
 </context>
 <context>
@@ -30,36 +22,24 @@
         <translation>Welkom bij LXQt</translation>
     </message>
     <message>
-        <source>&lt;b&gt;Welcome to LXQt&lt;/b&gt;
-&lt;p&gt;
-Before starting to use the LXQt, you might want to select the Windows Manager:</source>
-        <translation type="vanished">&lt;b&gt;Welkom bij LXQt&lt;/b&gt;⏎
-&lt;p&gt;⏎
-Voordat u gebruik maakt van LXQt-Qt, wilt u wellicht een Window Manager selecteren:</translation>
-    </message>
-    <message>
-        <source>You can change the Window Manager later at any time via LXQt Session Configurator.</source>
-        <translation type="vanished">U kunt ook op een later tijdstip van Window Manager wisselen, via de LXQt Sessie Configurator.</translation>
+        <location filename="../../../src/wmselectdialog.ui" line="20"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welkom bij LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Kies a.u.b. uw standaard-vensterbeheerder.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.cpp" line="62"/>
         <source>Other ...</source>
-        <translation>Overige ...</translation>
+        <translation>Overige...</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.cpp" line="63"/>
         <source>Choose your favorite one.</source>
-        <translation>Kies uw favoriete.</translation>
-    </message>
-    <message>
-        <location filename="../../../src/wmselectdialog.ui" line="20"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Kies uw favoriet.</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.ui" line="71"/>
         <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>U kunt dit te allen tijde veranderen via Voorkeuren -&gt; Sessie-instellingen -&gt; Basisinstellingen.</translation>
     </message>
 </context>
 </TS>
