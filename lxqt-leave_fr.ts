b82d1a479c373f81a1888173849b0cb27c858725
paiiou <paiiou@free.fr>
2016-10-19 10:18:38 +0200

    Update French translations
    liblxqt, about, notificationd, openssh-askpass, policykit,
    powermanagement, runner, session, sudo

---
+++ lxqt-leave_fr.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="../../leavedialog.ui" line="42"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="../../leavedialog.ui" line="59"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Que doit faire l&apos;ordinateur ?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../leavedialog.ui" line="92"/>
+        <source>Logout</source>
+        <translation>Déconnexion</translation>
+    </message>
+    <message>
+        <location filename="../../leavedialog.ui" line="112"/>
+        <source>Lock screen</source>
+        <translation>Verrouiller l&apos;écran</translation>
+    </message>
+    <message>
+        <location filename="../../leavedialog.ui" line="139"/>
+        <source>Suspend</source>
+        <translation>Suspendre</translation>
+    </message>
+    <message>
+        <location filename="../../leavedialog.ui" line="159"/>
+        <source>Hibernate</source>
+        <translation>Hiberner</translation>
+    </message>
+    <message>
+        <location filename="../../leavedialog.ui" line="186"/>
+        <source>Shutdown</source>
+        <translation>Arrêter</translation>
+    </message>
+    <message>
+        <location filename="../../leavedialog.ui" line="206"/>
+        <source>Reboot</source>
+        <translation>Redémarrer</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../main.cpp" line="51"/>
+        <source>Logout.</source>
+        <translation>Déconnexion.</translation>
+    </message>
+    <message>
+        <location filename="../../main.cpp" line="54"/>
+        <source>Lockscreen.</source>
+        <translation>Verrouillage de l&apos;écran.</translation>
+    </message>
+    <message>
+        <location filename="../../main.cpp" line="57"/>
+        <source>Suspend.</source>
+        <translation>Suspendre.</translation>
+    </message>
+    <message>
+        <location filename="../../main.cpp" line="60"/>
+        <source>Hibernate.</source>
+        <translation>Hibernation.</translation>
+    </message>
+    <message>
+        <location filename="../../main.cpp" line="63"/>
+        <source>Shutdown.</source>
+        <translation>Arrêt.</translation>
+    </message>
+    <message>
+        <location filename="../../main.cpp" line="66"/>
+        <source>Reboot.</source>
+        <translation>Redémarrage.</translation>
+    </message>
+</context>
+</TS>
