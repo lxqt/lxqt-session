cf6a331904b6f285ffecdd32c49bc73f20f574a0
scootergrisen <scootergrisen@gmail.com>
2016-12-14 02:46:15 +0100

    Update Danish translations

---
+++ lxqt-leave_da.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../../../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Forlad</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="59"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Hvad vil du have computeren til at gøre?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="92"/>
+        <source>Logout</source>
+        <translation>Log ud</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="206"/>
+        <source>Reboot</source>
+        <translation>Genstart</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="186"/>
+        <source>Shutdown</source>
+        <translation>Luk ned</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="139"/>
+        <source>Suspend</source>
+        <translation>Hvile</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="112"/>
+        <source>Lock screen</source>
+        <translation>Lås skærmen</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="159"/>
+        <source>Hibernate</source>
+        <translation>Dvale</translation>
+    </message>
+    <message>
+        <location filename="../../../leavedialog.ui" line="42"/>
+        <source>Cancel</source>
+        <translation>Annuller</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../../main.cpp" line="51"/>
+        <source>Logout.</source>
+        <translation>Log ud.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="54"/>
+        <source>Lockscreen.</source>
+        <translation>Lås skærmen.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="57"/>
+        <source>Suspend.</source>
+        <translation>Hvile.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="60"/>
+        <source>Hibernate.</source>
+        <translation>Dvale.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="63"/>
+        <source>Shutdown.</source>
+        <translation>Luk ned.</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="66"/>
+        <source>Reboot.</source>
+        <translation>Genstart.</translation>
+    </message>
+</context>
+</TS>
