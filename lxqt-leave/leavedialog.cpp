9368e7f032b308554752c698b98c79d4d21a1ad0
Palo Kisa <palo.kisa@gmail.com>
2017-03-23 10:31:48 +0100

    fixup...selection + unified size

--- lxqt-leave/leavedialog.cpp
+++ lxqt-leave/leavedialog.cpp
@@ -33,7 +33,7 @@
 class ItemDelegate : public QItemDelegate
 {
 public:
-    static constexpr QMargins MARGINS{20, 10, 20, 10};
+    static constexpr QMargins MARGINS{10, 10, 10, 10};
 public:
     using QItemDelegate::QItemDelegate;
     virtual QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override
@@ -44,13 +44,44 @@ public:
         size += {MARGINS.left() + MARGINS.right(), MARGINS.top() + MARGINS.bottom()}; // add some margins
         return size;
     }
+
     virtual void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override
     {
         QStyleOptionViewItem opt = option;
         opt.decorationPosition = QStyleOptionViewItem::Top;
         opt.displayAlignment = Qt::AlignHCenter | Qt::AlignTop;
-        opt.rect -= MARGINS;
         return QItemDelegate::paint(painter, opt, index);
+    }
+
+protected:
+    // Note: We want to paint the focus rectangle around the whole (text+icon)
+    //  (default in QItemDelegate is to draw the focus only in text rectangle)
+    virtual void drawFocus(QPainter *painter
+            , const QStyleOptionViewItem &option
+            , const QRect &/*rect*/) const override
+    {
+        // don't override the rectangle to the text-only
+        return QItemDelegate::drawFocus(painter, option, option.rect);
+    }
+
+    virtual void drawDisplay(QPainter *painter
+            , const QStyleOptionViewItem &option
+            , const QRect &rect
+            , const QString &text) const override
+    {
+        // shrink (and move to bottom) the text rectangle
+        QRect r = rect.adjusted(0, MARGINS.top(), 0, 0);
+        return QItemDelegate::drawDisplay(painter, option, r, text);
+    }
+
+    virtual void drawDecoration(QPainter *painter
+            , const QStyleOptionViewItem &option
+            , const QRect &rect
+            , const QPixmap &pixmap) const override
+    {
+        // move to bottom the pixmap rectangle
+        QRect r = rect.translated(0, MARGINS.top());
+        return QItemDelegate::drawDecoration(painter, option, r, pixmap);
     }
 };
 constexpr QMargins ItemDelegate::MARGINS;
@@ -84,38 +115,56 @@ LeaveDialog::LeaveDialog(QWidget* parent)
     // populate the items
     ui->tableWidget->setColumnCount(3);
     ui->tableWidget->setRowCount(2);
-    QTableWidgetItem * item = new QTableWidgetItem{QIcon::fromTheme("system-log-out"), tr("Logout")};
+    QTableWidgetItem * item = new QTableWidgetItem{QIcon::fromTheme(QStringLiteral("system-log-out")), tr("Logout")};
     item->setData(Qt::UserRole, LXQt::Power::PowerLogout);
     if (!mPower->canAction(LXQt::Power::PowerLogout))
         item->setFlags(item->flags() & ~Qt::ItemIsEnabled);
     ui->tableWidget->setItem(0, 0, item);
-    item = new QTableWidgetItem{QIcon::fromTheme("system-shutdown"), tr("Shutdown")};
+    item = new QTableWidgetItem{QIcon::fromTheme(QStringLiteral("system-shutdown")), tr("Shutdown")};
     item->setData(Qt::UserRole, LXQt::Power::PowerShutdown);
     if (!mPower->canAction(LXQt::Power::PowerShutdown))
         item->setFlags(item->flags() & ~Qt::ItemIsEnabled);
     ui->tableWidget->setItem(0, 1, item);
-    item = new QTableWidgetItem{QIcon::fromTheme("system-suspend"), tr("Suspend")};
+    item = new QTableWidgetItem{QIcon::fromTheme(QStringLiteral("system-suspend")), tr("Suspend")};
     item->setData(Qt::UserRole, LXQt::Power::PowerSuspend);
     if (!mPower->canAction(LXQt::Power::PowerSuspend))
         item->setFlags(item->flags() & ~Qt::ItemIsEnabled);
     ui->tableWidget->setItem(0, 2, item);
-    item = new QTableWidgetItem{QIcon::fromTheme("system-lock-screen"), tr("Lock screen")};
+    item = new QTableWidgetItem{QIcon::fromTheme(QStringLiteral("system-lock-screen")), tr("Lock screen")};
     item->setData(Qt::UserRole, -1);
     ui->tableWidget->setItem(1, 0, item);
-    item = new QTableWidgetItem{QIcon::fromTheme("system-reboot"), tr("Reboot")};
+    item = new QTableWidgetItem{QIcon::fromTheme(QStringLiteral("system-reboot")), tr("Reboot")};
     item->setData(Qt::UserRole, LXQt::Power::PowerReboot);
     if (!mPower->canAction(LXQt::Power::PowerReboot))
         item->setFlags(item->flags() & ~Qt::ItemIsEnabled);
     ui->tableWidget->setItem(1, 1, item);
-    item = new QTableWidgetItem{QIcon::fromTheme("system-suspend-hibernate"), tr("Hibernate")};
+    item = new QTableWidgetItem{QIcon::fromTheme(QStringLiteral("system-suspend-hibernate")), tr("Hibernate")};
     item->setData(Qt::UserRole, LXQt::Power::PowerHibernate);
     if (!mPower->canAction(LXQt::Power::PowerHibernate))
         item->setFlags(item->flags() & ~Qt::ItemIsEnabled);
     ui->tableWidget->setItem(1, 2, item);
 
-    connect(ui->tableWidget, &QTableWidget::itemActivated, this, [this] (QTableWidgetItem *item) {
+    // set unified size for all cells
+    int height = 0;
+    for (int row = 0; row < ui->tableWidget->rowCount(); ++row)
+        height = qMax(height, ui->tableWidget->rowHeight(row));
+    int width = 0;
+    for (int col = 0; col < ui->tableWidget->columnCount(); ++col)
+        width = qMax(width, ui->tableWidget->columnWidth(col));
+    for (int row = 0; row < ui->tableWidget->rowCount(); ++row)
+    {
+        ui->tableWidget->verticalHeader()->setSectionResizeMode(row, QHeaderView::Fixed);
+        ui->tableWidget->setRowHeight(row, height);
+    }
+    for (int col = 0; col < ui->tableWidget->columnCount(); ++col)
+    {
+        ui->tableWidget->horizontalHeader()->setSectionResizeMode(col, QHeaderView::Fixed);
+        ui->tableWidget->setColumnWidth(col, width);
+    }
+
+    connect(ui->tableWidget, &QAbstractItemView::activated, this, [this] (const QModelIndex & index) {
         bool ok = false;
-        const int action = item->data(Qt::UserRole).toInt(&ok);
+        const int action = index.data(Qt::UserRole).toInt(&ok);
         if (!ok)
         {
             qWarning("Invalid internal logic, no UserRole set!?");
