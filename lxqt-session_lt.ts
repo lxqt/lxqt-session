30568df611166f52a72cf0c293570bf118242ef4
welaq <hazap@hotmail.com>
2017-05-23 02:18:44 +0300

    Lithuanian translatio

--- lxqt-session_lt.ts
+++ lxqt-session_lt.ts
@@ -4,22 +4,14 @@
 <context>
     <name>LXQtModuleManager</name>
     <message>
-        <source>LXQt Session Crash Report</source>
-        <translation type="vanished">Pranešimas apie LXQt sesijų lūžimus</translation>
-    </message>
-    <message>
-        <source>Application &apos;%1&apos; crashed too many times. Its autorestart has been disabled for current session.</source>
-        <translation type="vanished">Programa „%1“ dažnai nulūžta. Jos automatinis paleidimas išjungiamas šiai sesijai.</translation>
-    </message>
-    <message>
         <location filename="../../../src/lxqtmodman.cpp" line="302"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Strigties ataskaita</translation>
     </message>
     <message>
         <location filename="../../../src/lxqtmodman.cpp" line="303"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; užstrigo pernelyg daug kartų. Programos automatinis paleidimas iš naujo yra išjungtas iki kito prisijungimo.</translation>
     </message>
 </context>
 <context>
@@ -27,19 +19,17 @@
     <message>
         <location filename="../../../src/wmselectdialog.ui" line="14"/>
         <source>Welcome to LXQt</source>
-        <translation>Jus sveikina LXQt</translation>
+        <translation>Sveiki atvykę į LXQt</translation>
     </message>
     <message>
-        <source>&lt;b&gt;Welcome to LXQt&lt;/b&gt;
-&lt;p&gt;
-Before starting to use the LXQt, you might want to select the Windows Manager:</source>
-        <translation type="vanished">&lt;b&gt;Jus sveikina LXQt&lt;/b&gt;
-&lt;p&gt;
-Prieš pradėdami dirbti LXQt aplinkoje, pasirinkite norimą langų tvarkyklę.</translation>
+        <location filename="../../../src/wmselectdialog.ui" line="20"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Sveiki atvykę į LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Prašome pasirinkti savo numatytąją langų tvarkytuvę.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <source>You can change the Window Manager later at any time via LXQt Session Configurator.</source>
-        <translation type="vanished">Langų tvarkyklę bet kada vėliau galėsite pasikeisti LXQt sesijų konfigūracijoje.</translation>
+        <location filename="../../../src/wmselectdialog.ui" line="71"/>
+        <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
+        <translation>Jūs tai galėsite pakeisti bet kuriuo metu per Nuostatos -&gt; Seanso nustatymai -&gt; Pagrindiniai nustatymai. </translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.cpp" line="62"/>
@@ -49,17 +39,7 @@ Prieš pradėdami dirbti LXQt aplinkoje, pasirinkite norimą langų tvarkyklę.<
     <message>
         <location filename="../../../src/wmselectdialog.cpp" line="63"/>
         <source>Choose your favorite one.</source>
-        <translation>Pasirinkite norimą.</translation>
-    </message>
-    <message>
-        <location filename="../../../src/wmselectdialog.ui" line="20"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../src/wmselectdialog.ui" line="71"/>
-        <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pasirinkite savo mėgstamą.</translation>
     </message>
 </context>
 </TS>
