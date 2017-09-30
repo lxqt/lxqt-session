eb986e149bbf38062581890fc9a3cafcea78dc6f
Arif Budiman <arifpedia@gmail.com>
2017-10-01 01:20:06 +0700

    Indonesian translation (#260)
    * Update compton-conf_id.ts
    * Create libfm-qt_id.ts
    * Added liblxqt/liblxqt_id.ts
    * Added lximage-qt/lximage-qt_id.ts
    * Added lxqt-about/lxqt-about_id.ts
    * Added lxqt-admin/lxqt-admin-time/lxqt-admin-time_id.ts
    * Added lxqt-admin/lxqt-admin-user/lxqt-admin-user_id.ts
    * Added lxqt-config/liblxqt-config-cursor/lxqt-config-cursor_id.ts
    * Added lxqt-config/lxqt-config/lxqt-config_id.ts
    * Added lxqt-config/lxqt-config-appearance/lxqt-config-appearance_id.ts
    * Added lxqt-config/lxqt-config-brightness/lxqt-config-brightness_id.ts
    * Added lxqt-config/lxqt-config-file-associations/lxqt-config-file-associations_id.ts
    * Update lxqt-config/lxqt-config-input/lxqt-config-input_id.ts
    * Added lxqt-config/lxqt-config-locale/lxqt-config-locale_id.ts
    * Added lxqt-config/lxqt-config-monitor/lxqt-config-monitor_id.ts
    * Added lxqt-config-globalkeyshortcuts_id.ts
    * Added lxqt-config-notificationd_id.ts
    * Added lxqt-notificationd_id.ts
    * Added lxqt-openssh-askpass_id.ts
    * Added lxqt-panel_id.ts
    * Added clock_id.ts
    * Added cpuload_id.ts
    * Added desktopswitch_id.ts
    * Added directorymenu_id.ts
    * Added dom_id.ts
    * Added kbindicator_id.ts
    * Added mainmenu_id.ts
    * Added mount_id.ts
    * Added networkmonitor_id.ts
    * Added quicklaunch_id.ts
    * Added sensors_id.ts
    * Added showdesktop_id.ts
    * Added spacer_id.ts
    * Added sysstat_id.ts
    * Added taskbar_id.ts
    * Added volume_id.ts
    * Added worldclock_id.ts
    * Added lxqt-policykit-agent_id.ts
    * Added lxqt-config-powermanagement_id.ts
    * Added lxqt-powermanagement_id.ts
    * Added lxqt-runner_id.ts
    * Added lxqt-config-session_id.ts
    * Added lxqt-leave_id.ts
    * Added lxqt-session_id.ts
    * Added lxqt-sudo_id.ts
    * Remove all Indonesian translation files using "id_ID"

--- lxqt-session_id.ts
+++ lxqt-session_id.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="id_ID">
+<TS version="2.0" language="id">
 <context>
     <name>LXQtModuleManager</name>
     <message>
         <location filename="../../../src/lxqtmodman.cpp" line="305"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Laporan Kerusakan</translation>
     </message>
     <message>
         <location filename="../../../src/lxqtmodman.cpp" line="306"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; terlalu sering rusak. Memulai otomatisnya telah dinonaktifkan sampai masuk log berikutnya.</translation>
     </message>
 </context>
 <context>
@@ -19,27 +19,27 @@
     <message>
         <location filename="../../../src/wmselectdialog.cpp" line="62"/>
         <source>Other ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Lainnya ...</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.cpp" line="63"/>
         <source>Choose your favorite one.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pilih yang favorit.</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.ui" line="14"/>
         <source>Welcome to LXQt</source>
-        <translation type="unfinished"></translation>
+        <translation>Selamat datang di LXQt</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Selamat datang di LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Silakan pilih Manajer Jendela standar Anda.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../../src/wmselectdialog.ui" line="71"/>
         <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Anda akan dapat mengubahnya kapan saja melalui Preferensi -&gt; Pengaturan Sesi -&gt; Pengaturan Dasar.</translation>
     </message>
 </context>
 </TS>
