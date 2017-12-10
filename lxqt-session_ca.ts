30b43de8ec47244b55acdf530c4822af2b3baf0a
rbuj <robert.buj@gmail.com>
2017-12-10 09:51:52 +0100

    Update Catalan translation

--- lxqt-session_ca.ts
+++ lxqt-session_ca.ts
@@ -19,18 +19,18 @@
     <message>
         <location filename="../../../src/main.cpp" line="58"/>
         <source>Configuration file path.</source>
-        <translation type="unfinished"></translation>
+        <translation>Camí al fitxer de configuració.</translation>
     </message>
     <message>
         <location filename="../../../src/main.cpp" line="58"/>
         <location filename="../../../src/main.cpp" line="59"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>fitxer</translation>
     </message>
     <message>
         <location filename="../../../src/main.cpp" line="59"/>
         <source>Window manager to use.</source>
-        <translation type="unfinished"></translation>
+        <translation>Gestor de finestres a utilitzar.</translation>
     </message>
 </context>
 <context>
