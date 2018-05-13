cda427ebb2c6d59551b4bc9d649030a32121c9ae
micrococo <micrococo@gmx.com>
2018-05-13 10:59:05 +0200

    Spanish translation review

--- lxqt-leave_es.ts
+++ lxqt-leave_es.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../../../leavedialog.ui" line="71"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;¿Qué desea hacer con el computador?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;¿Qué desea hacer con el sistema?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../../leavedialog.cpp" line="51"/>
