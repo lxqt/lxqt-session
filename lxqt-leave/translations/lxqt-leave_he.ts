6481cb8e013129f87eea83002153af5777fc4ed1
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-19 11:32:42 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Session (Leave)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-leave/he/

--- lxqt-leave/translations/lxqt-leave_he.ts
+++ lxqt-leave/translations/lxqt-leave_he.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="../leavedialog.ui" line="20"/>
         <source>Leave</source>
-        <translation></translation>
+        <translation>עזיבה</translation>
     </message>
     <message>
         <location filename="../leavedialog.ui" line="71"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;מה ברצונך להורות למחשב לבצע?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="51"/>
         <source>Logout</source>
-        <translation></translation>
+        <translation>לצאת</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="69"/>
         <source>Reboot</source>
-        <translation></translation>
+        <translation>להפעיל מחדש</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="56"/>
         <source>Shutdown</source>
-        <translation></translation>
+        <translation>לכבות</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="61"/>
         <source>Suspend</source>
-        <translation></translation>
+        <translation>להשהות</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="66"/>
         <source>Lock screen</source>
-        <translation></translation>
+        <translation>לנעול מסך</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="74"/>
         <source>Hibernate</source>
-        <translation></translation>
+        <translation>להרדם</translation>
     </message>
     <message>
         <location filename="../leavedialog.ui" line="51"/>
         <source>Cancel</source>
-        <translation></translation>
+        <translation>ביטול</translation>
     </message>
 </context>
 <context>
@@ -54,32 +54,32 @@
     <message>
         <location filename="../main.cpp" line="58"/>
         <source>Logout.</source>
-        <translation></translation>
+        <translation>יציאה.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="61"/>
         <source>Lockscreen.</source>
-        <translation></translation>
+        <translation>מסך נעילה.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="64"/>
         <source>Suspend.</source>
-        <translation></translation>
+        <translation>השהיה.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="67"/>
         <source>Hibernate.</source>
-        <translation></translation>
+        <translation>תרדמת.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="70"/>
         <source>Shutdown.</source>
-        <translation></translation>
+        <translation>כיבוי.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="73"/>
         <source>Reboot.</source>
-        <translation></translation>
+        <translation>הפעלה מחדש.</translation>
     </message>
 </context>
 </TS>
