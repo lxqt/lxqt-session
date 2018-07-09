d2ce8198c01c85ef0bcbf7df672df0ebb9f90508
Lucas A. V. Dantas <lucasdantas@yandex.com>
2018-07-09 16:19:23 +0000

    Translated using Weblate (Português (Brasil))
    Currently translated at 100,0% (10 of 10 strings)
    
    Translation: LXQt/LXQt Session
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-session/pt_BR/

--- lxqt-session/translations/lxqt-session_pt_BR.ts
+++ lxqt-session/translations/lxqt-session_pt_BR.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/lxqtmodman.cpp" line="310"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Relatório de travamentos</translation>
     </message>
     <message>
         <location filename="../src/lxqtmodman.cpp" line="311"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; travou demais. A sua execução automática foi desativada até a próxima sessão.</translation>
     </message>
 </context>
 <context>
@@ -19,18 +19,18 @@
     <message>
         <location filename="../src/main.cpp" line="59"/>
         <source>Configuration file path.</source>
-        <translation type="unfinished"></translation>
+        <translation>Caminho do arquivo de configuração.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="59"/>
         <location filename="../src/main.cpp" line="60"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>arquivo</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="60"/>
         <source>Window manager to use.</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerenciador de arquivos a ser usado.</translation>
     </message>
 </context>
 <context>
@@ -53,12 +53,12 @@
     <message>
         <location filename="../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Benvindo ao LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Por favor, selecione o gerenciador de janelas padrão.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="71"/>
         <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Você pode configurar isso a qualquer momento indo a Preferências -&gt; Configurações de sessão -&gt; Configurações básicas.</translation>
     </message>
 </context>
 </TS>
