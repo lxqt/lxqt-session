d7e2bd61efcd7330506e3eba76f136367b10aa86
ButterflyOfFire <butterflyoffire+lxqt@protonmail.com>
2018-07-10 17:28:46 +0000

    Translated using Weblate (Arabe)
    Currently translated at 100,0% (10 of 10 strings)
    
    Translation: LXQt/LXQt Session
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-session/ar/

--- lxqt-session/translations/lxqt-session_ar.ts
+++ lxqt-session/translations/lxqt-session_ar.ts
@@ -19,18 +19,18 @@
     <message>
         <location filename="../src/main.cpp" line="59"/>
         <source>Configuration file path.</source>
-        <translation type="unfinished"></translation>
+        <translation>مسار ملف الإعداد.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="59"/>
         <location filename="../src/main.cpp" line="60"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>ملف</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="60"/>
         <source>Window manager to use.</source>
-        <translation type="unfinished"></translation>
+        <translation>مدير النوافذ الذي تود استخدامه.</translation>
     </message>
 </context>
 <context>
