b535bd4d7a730e5994ce7e7ec54a0fd72a535649
p-bo <pavel.borecki@gmail.com>
2018-07-11 06:31:48 +0000

    Translated using Weblate (Čeština)
    Currently translated at 100,0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Session (Leave)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-leave/cs/

--- lxqt-leave/translations/lxqt-leave_cs.ts
+++ lxqt-leave/translations/lxqt-leave_cs.ts
@@ -64,7 +64,7 @@
     <message>
         <location filename="../main.cpp" line="64"/>
         <source>Suspend.</source>
-        <translation>Uspat.</translation>
+        <translation>Uspat do operační paměti.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="67"/>
