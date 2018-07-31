6aebf0f2d4432249fe135d4cfcfce8992e79aa7d
Simon Quigley <tsimonq2@ubuntu.com>
2018-07-31 19:31:27 +0000

    Added translation using Weblate (Welsh)

---
+++ lxqt-config-session/translations/lxqt-config-session_cy.ts
@@ -0,0 +1,368 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cy">
+<context>
+    <name>AutoStartEdit</name>
+    <message>
+        <location filename="../autostartedit.ui" line="14"/>
+        <source>Edit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.ui" line="20"/>
+        <source>Name:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.ui" line="27"/>
+        <source>Search...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.ui" line="47"/>
+        <source>Command:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.ui" line="57"/>
+        <source>Wait for system tray</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.cpp" line="59"/>
+        <source>Select Application</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>AutoStartItemModel</name>
+    <message>
+        <location filename="../autostartmodel.cpp" line="191"/>
+        <source>Global Autostart</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartmodel.cpp" line="193"/>
+        <source>LXQt Autostart</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartmodel.cpp" line="211"/>
+        <source>Location: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartmodel.cpp" line="213"/>
+        <source>Overrides: %1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>AutoStartPage</name>
+    <message>
+        <location filename="../autostartpage.ui" line="17"/>
+        <source>Delete</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.ui" line="24"/>
+        <source>Add</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.ui" line="31"/>
+        <source>Edit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.ui" line="44"/>
+        <source>Application Autostart</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.cpp" line="141"/>
+        <source>Error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.cpp" line="141"/>
+        <source>File &apos;%1&apos; already exists!</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="30"/>
+        <source>Window Manager</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="49"/>
+        <source>Search...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="59"/>
+        <source>LXQt Modules</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="65"/>
+        <source>Start</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="72"/>
+        <source>Stop</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="111"/>
+        <source>Leave Session</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="117"/>
+        <source>Ask for confirmation to leave session</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="124"/>
+        <source>Lock screen before suspending/hibernating</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="147"/>
+        <source>Select a window manager</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>DefaultApps</name>
+    <message>
+        <location filename="../defaultappspage.cpp" line="72"/>
+        <source>Select a terminal emulator</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../defaultappspage.cpp" line="77"/>
+        <source>Select a web browser</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>DefaultAppsPage</name>
+    <message>
+        <location filename="../defaultappspage.ui" line="23"/>
+        <source>Default Applications</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../defaultappspage.ui" line="32"/>
+        <source>Terminal Emulation:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../defaultappspage.ui" line="52"/>
+        <location filename="../defaultappspage.ui" line="79"/>
+        <source>Search...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../defaultappspage.ui" line="59"/>
+        <source>Web Browser:</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>EnvironmentPage</name>
+    <message>
+        <location filename="../environmentpage.ui" line="17"/>
+        <source>Add</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../environmentpage.ui" line="24"/>
+        <source>Delete</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../environmentpage.ui" line="50"/>
+        <source>Environment Variables (Advanced)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../environmentpage.ui" line="61"/>
+        <source>Variable Name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../environmentpage.ui" line="66"/>
+        <source>Value</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ModuleModel</name>
+    <message>
+        <location filename="../modulemodel.cpp" line="86"/>
+        <source>Running</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>SessionConfigWindow</name>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="44"/>
+        <source>LXQt Session Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="47"/>
+        <source>Basic Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="53"/>
+        <source>Default Applications</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="56"/>
+        <source>User Directories</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="62"/>
+        <source>Autostart</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="68"/>
+        <source>Environment (Advanced)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="94"/>
+        <source>Session Restart Required</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="95"/>
+        <source>Some settings will not take effect until the next log in.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>UserLocationsPage</name>
+    <message>
+        <location filename="../userlocationspage.cpp" line="110"/>
+        <source>Locations for Personal Files</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="179"/>
+        <source>LXQt Session Settings - User Directories</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="180"/>
+        <source>An error ocurred while applying the settings for the %1 location</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="196"/>
+        <source>Choose Location</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>UserLocationsPrivate</name>
+    <message>
+        <location filename="../userlocationspage.cpp" line="60"/>
+        <source>Desktop</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="61"/>
+        <source>Downloads</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="62"/>
+        <source>Templates</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="63"/>
+        <source>Public Share</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="64"/>
+        <source>Documents</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="65"/>
+        <source>Music</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="66"/>
+        <source>Pictures</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="67"/>
+        <source>Videos</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="70"/>
+        <source>Contains all the files which you see on your desktop</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="71"/>
+        <source>Default folder to save your downloaded files</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="72"/>
+        <source>Default folder to load or save templates from or to</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="73"/>
+        <source>Default folder to publicly share your files</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="74"/>
+        <source>Default folder to load or save documents from or to</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="75"/>
+        <source>Default foldet to load or save music from or to</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="76"/>
+        <source>Default folder to load or save pictures from or to</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="77"/>
+        <source>Default folder to load or save videos from or to</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
