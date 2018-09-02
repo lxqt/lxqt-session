2961df20b1143744510dced35d324614228a0b86
Carmina16 <mistresssilvara@hotmail.com>
2018-09-02 12:53:03 +0000

    Translated using Weblate (Russian)
    Currently translated at 100,0% (10 of 10 strings)
    
    Translation: LXQt/LXQt Session
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-session/ru/

--- lxqt-session/translations/lxqt-session_ru.ts
+++ lxqt-session/translations/lxqt-session_ru.ts
@@ -19,18 +19,18 @@
     <message>
         <location filename="../src/main.cpp" line="59"/>
         <source>Configuration file path.</source>
-        <translation type="unfinished"></translation>
+        <translation>Путь к файлу конфигурации.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="59"/>
         <location filename="../src/main.cpp" line="60"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>файл</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="60"/>
         <source>Window manager to use.</source>
-        <translation type="unfinished"></translation>
+        <translation>Используемый менеджер окон.</translation>
     </message>
 </context>
 <context>
