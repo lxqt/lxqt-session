944dd3439aa01541cf467d5950c015c9a4ede44f
Einar Mostad <einar@mostad.eu>
2018-11-19 23:37:15 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (66 of 66 strings)
    
    Translation: LXQt/LXQt Session (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-session/nb_NO/

--- lxqt-config-session/translations/lxqt-config-session_nb_NO.ts
+++ lxqt-config-session/translations/lxqt-config-session_nb_NO.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../autostartedit.ui" line="14"/>
         <source>Edit</source>
-        <translation></translation>
+        <translation>Rediger</translation>
     </message>
     <message>
         <location filename="../autostartedit.ui" line="20"/>
         <source>Name:</source>
-        <translation></translation>
+        <translation>Navn:</translation>
     </message>
     <message>
         <location filename="../autostartedit.ui" line="27"/>
         <source>Search...</source>
-        <translation></translation>
+        <translation>Søk..</translation>
     </message>
     <message>
         <location filename="../autostartedit.ui" line="47"/>
         <source>Command:</source>
-        <translation></translation>
+        <translation>Kommando:</translation>
     </message>
     <message>
         <location filename="../autostartedit.ui" line="57"/>
         <source>Wait for system tray</source>
-        <translation></translation>
+        <translation>Vent på systemtrau</translation>
     </message>
     <message>
         <location filename="../autostartedit.cpp" line="61"/>
         <source>Select Application</source>
-        <translation></translation>
+        <translation>Velg program</translation>
     </message>
 </context>
 <context>
@@ -39,22 +39,22 @@
     <message>
         <location filename="../autostartmodel.cpp" line="193"/>
         <source>Global Autostart</source>
-        <translation></translation>
+        <translation>Global autostart</translation>
     </message>
     <message>
         <location filename="../autostartmodel.cpp" line="195"/>
         <source>LXQt Autostart</source>
-        <translation></translation>
+        <translation>LXQt autostart</translation>
     </message>
     <message>
         <location filename="../autostartmodel.cpp" line="213"/>
         <source>Location: %1</source>
-        <translation></translation>
+        <translation>Sted: %1</translation>
     </message>
     <message>
         <location filename="../autostartmodel.cpp" line="215"/>
         <source>Overrides: %1</source>
-        <translation></translation>
+        <translation>Gjort om: %1</translation>
     </message>
 </context>
 <context>
@@ -62,32 +62,32 @@
     <message>
         <location filename="../autostartpage.ui" line="17"/>
         <source>Delete</source>
-        <translation></translation>
+        <translation>Slett</translation>
     </message>
     <message>
         <location filename="../autostartpage.ui" line="24"/>
         <source>Add</source>
-        <translation></translation>
+        <translation>Legg til</translation>
     </message>
     <message>
         <location filename="../autostartpage.ui" line="31"/>
         <source>Edit</source>
-        <translation></translation>
+        <translation>Rediger</translation>
     </message>
     <message>
         <location filename="../autostartpage.ui" line="44"/>
         <source>Application Autostart</source>
-        <translation></translation>
+        <translation>Autostart programmer</translation>
     </message>
     <message>
         <location filename="../autostartpage.cpp" line="143"/>
         <source>Error</source>
-        <translation></translation>
+        <translation>Feil</translation>
     </message>
     <message>
         <location filename="../autostartpage.cpp" line="143"/>
         <source>File &apos;%1&apos; already exists!</source>
-        <translation></translation>
+        <translation>Filen &apos;%1&apos; finnes allerede!</translation>
     </message>
 </context>
 <context>
@@ -95,22 +95,22 @@
     <message>
         <location filename="../basicsettings.ui" line="23"/>
         <source>Basic Settings</source>
-        <translation></translation>
+        <translation>Grunnleggende innstillinger</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="30"/>
         <source>Window Manager</source>
-        <translation></translation>
+        <translation>Vindushåndterer</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="49"/>
         <source>Search...</source>
-        <translation></translation>
+        <translation>Søk...</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="59"/>
         <source>LXQt Modules</source>
-        <translation></translation>
+        <translation>LXQt-moduler</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="65"/>
@@ -120,27 +120,27 @@
     <message>
         <location filename="../basicsettings.ui" line="72"/>
         <source>Stop</source>
-        <translation></translation>
+        <translation>Stopp</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="111"/>
         <source>Leave Session</source>
-        <translation></translation>
+        <translation>Logg ut</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="117"/>
         <source>Ask for confirmation to leave session</source>
-        <translation></translation>
+        <translation>Spør om bekreftelse for å logge ut</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="124"/>
         <source>Lock screen before suspending/hibernating</source>
-        <translation></translation>
+        <translation>Lås skjerm før dvale eller dyp dvale</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="147"/>
         <source>Select a window manager</source>
-        <translation></translation>
+        <translation>Velg en vindushåndterer</translation>
     </message>
 </context>
 <context>
@@ -148,12 +148,12 @@
     <message>
         <location filename="../defaultappspage.cpp" line="74"/>
         <source>Select a terminal emulator</source>
-        <translation></translation>
+        <translation>Velg en terminalemulator</translation>
     </message>
     <message>
         <location filename="../defaultappspage.cpp" line="79"/>
         <source>Select a web browser</source>
-        <translation></translation>
+        <translation>Velg en nettleser</translation>
     </message>
 </context>
 <context>
@@ -161,23 +161,23 @@
     <message>
         <location filename="../defaultappspage.ui" line="23"/>
         <source>Default Applications</source>
-        <translation></translation>
+        <translation>Standardprogrammer</translation>
     </message>
     <message>
         <location filename="../defaultappspage.ui" line="32"/>
         <source>Terminal Emulation:</source>
-        <translation></translation>
+        <translation>Terminalemulering:</translation>
     </message>
     <message>
         <location filename="../defaultappspage.ui" line="52"/>
         <location filename="../defaultappspage.ui" line="79"/>
         <source>Search...</source>
-        <translation></translation>
+        <translation>Søk...</translation>
     </message>
     <message>
         <location filename="../defaultappspage.ui" line="59"/>
         <source>Web Browser:</source>
-        <translation></translation>
+        <translation>Nettleser:</translation>
     </message>
 </context>
 <context>
@@ -185,27 +185,27 @@
     <message>
         <location filename="../environmentpage.ui" line="17"/>
         <source>Add</source>
-        <translation></translation>
+        <translation>Legg til</translation>
     </message>
     <message>
         <location filename="../environmentpage.ui" line="24"/>
         <source>Delete</source>
-        <translation></translation>
+        <translation>Slett</translation>
     </message>
     <message>
         <location filename="../environmentpage.ui" line="50"/>
         <source>Environment Variables (Advanced)</source>
-        <translation></translation>
+        <translation>Miljøvariabler (avansert)</translation>
     </message>
     <message>
         <location filename="../environmentpage.ui" line="61"/>
         <source>Variable Name</source>
-        <translation></translation>
+        <translation>Variabelnavn</translation>
     </message>
     <message>
         <location filename="../environmentpage.ui" line="66"/>
         <source>Value</source>
-        <translation></translation>
+        <translation>Verdi</translation>
     </message>
 </context>
 <context>
@@ -213,7 +213,7 @@
     <message>
         <location filename="../modulemodel.cpp" line="87"/>
         <source>Running</source>
-        <translation></translation>
+        <translation>Kjører</translation>
     </message>
 </context>
 <context>
@@ -221,22 +221,22 @@
     <message>
         <location filename="../sessionconfigwindow.cpp" line="45"/>
         <source>LXQt Session Settings</source>
-        <translation></translation>
+        <translation>LXQt Sesjonsinnstillinger</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="48"/>
         <source>Basic Settings</source>
-        <translation></translation>
+        <translation>Grunnleggende innstillinger</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="54"/>
         <source>Default Applications</source>
-        <translation></translation>
+        <translation>Standardprogrammer</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="57"/>
         <source>User Directories</source>
-        <translation></translation>
+        <translation>Brukerordlister</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="63"/>
@@ -246,17 +246,17 @@
     <message>
         <location filename="../sessionconfigwindow.cpp" line="69"/>
         <source>Environment (Advanced)</source>
-        <translation></translation>
+        <translation>Miljø (Avansert)</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="95"/>
         <source>Session Restart Required</source>
-        <translation></translation>
+        <translation>Omstart kreves</translation>
     </message>
     <message>
         <location filename="../sessionconfigwindow.cpp" line="96"/>
         <source>Some settings will not take effect until the next log in.</source>
-        <translation></translation>
+        <translation>Noen innstillinger vil ikke forandres før neste gang du logger inn.</translation>
     </message>
 </context>
 <context>
@@ -264,22 +264,22 @@
     <message>
         <location filename="../userlocationspage.cpp" line="116"/>
         <source>Locations for Personal Files</source>
-        <translation></translation>
+        <translation>Plassering av personlige filer</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="185"/>
         <source>LXQt Session Settings - User Directories</source>
-        <translation></translation>
+        <translation>LXQt sesjonsinnstillinger - brukerordlister</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="186"/>
         <source>An error ocurred while applying the settings for the %1 location</source>
-        <translation></translation>
+        <translation>En feil skjedde mens innstillingene for stedet %1 ble tatt i bruk.</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="202"/>
         <source>Choose Location</source>
-        <translation></translation>
+        <translation>Velg sted</translation>
     </message>
 </context>
 <context>
@@ -287,82 +287,82 @@
     <message>
         <location filename="../userlocationspage.cpp" line="63"/>
         <source>Desktop</source>
-        <translation></translation>
+        <translation>Skrivebord</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="64"/>
         <source>Downloads</source>
-        <translation></translation>
+        <translation>Nedlastinger</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="65"/>
         <source>Templates</source>
-        <translation></translation>
+        <translation>Maler</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="66"/>
         <source>Public Share</source>
-        <translation></translation>
+        <translation>Delt filområde</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="67"/>
         <source>Documents</source>
-        <translation></translation>
+        <translation>Dokumenter</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="68"/>
         <source>Music</source>
-        <translation></translation>
+        <translation>Musikk</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="69"/>
         <source>Pictures</source>
-        <translation></translation>
+        <translation>Bilder</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="70"/>
         <source>Videos</source>
-        <translation></translation>
+        <translation>Videoer</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="73"/>
         <source>Contains all the files which you see on your desktop</source>
-        <translation></translation>
+        <translation>Inneholder alle filene du ser på skrivebordet</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="74"/>
         <source>Default folder to save your downloaded files</source>
-        <translation></translation>
+        <translation>Standardmappe å lagre nedlastinger til</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="75"/>
         <source>Default folder to load or save templates from or to</source>
-        <translation></translation>
+        <translation>Standardmappe å hente maler fra eller lagre maler til</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="76"/>
         <source>Default folder to publicly share your files</source>
-        <translation></translation>
+        <translation>Standardmappe for offentlig deling av filer</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="77"/>
         <source>Default folder to load or save documents from or to</source>
-        <translation></translation>
+        <translation>Standardmappe å hente dokumenter fra eller lagre dokumenter til</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="78"/>
         <source>Default foldet to load or save music from or to</source>
-        <translation></translation>
+        <translation>Standardmappe å hente musikk fra eller lagre musikk til</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="79"/>
         <source>Default folder to load or save pictures from or to</source>
-        <translation></translation>
+        <translation>Standardmappe å hente bilder fra eller lagre bilder til</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="80"/>
         <source>Default folder to load or save videos from or to</source>
-        <translation></translation>
+        <translation>Standardmappe å hente videoer fra eller lagre videoer til</translation>
     </message>
 </context>
 </TS>
