bf4eca4da00ca7ee492b33b62d1b512e60d06895
Einar Mostad <einar@mostad.eu>
2018-11-20 00:25:38 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Session (Leave)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-leave/nb_NO/

--- lxqt-leave/translations/lxqt-leave_nb_NO.ts
+++ lxqt-leave/translations/lxqt-leave_nb_NO.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="../leavedialog.ui" line="20"/>
         <source>Leave</source>
-        <translation></translation>
+        <translation>Avslutt</translation>
     </message>
     <message>
         <location filename="../leavedialog.ui" line="71"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Hva vil du at maskinen skal gjøre?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="51"/>
         <source>Logout</source>
-        <translation></translation>
+        <translation>Logg ut</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="69"/>
         <source>Reboot</source>
-        <translation></translation>
+        <translation>Omstart</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="56"/>
         <source>Shutdown</source>
-        <translation></translation>
+        <translation>Skru av</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="61"/>
         <source>Suspend</source>
-        <translation></translation>
+        <translation>Dvale</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="66"/>
         <source>Lock screen</source>
-        <translation></translation>
+        <translation>Låseskjerm</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="74"/>
         <source>Hibernate</source>
-        <translation></translation>
+        <translation>Dyp dvale</translation>
     </message>
     <message>
         <location filename="../leavedialog.ui" line="51"/>
         <source>Cancel</source>
-        <translation></translation>
+        <translation>Avbryt</translation>
     </message>
 </context>
 <context>
@@ -54,32 +54,32 @@
     <message>
         <location filename="../main.cpp" line="58"/>
         <source>Logout.</source>
-        <translation></translation>
+        <translation>Logg ut.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="61"/>
         <source>Lockscreen.</source>
-        <translation></translation>
+        <translation>Låseskjerm.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="64"/>
         <source>Suspend.</source>
-        <translation></translation>
+        <translation>Dvale.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="67"/>
         <source>Hibernate.</source>
-        <translation></translation>
+        <translation>Dyp dvale.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="70"/>
         <source>Shutdown.</source>
-        <translation></translation>
+        <translation>Skru av.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="73"/>
         <source>Reboot.</source>
-        <translation></translation>
+        <translation>Omstart.</translation>
     </message>
 </context>
 </TS>
