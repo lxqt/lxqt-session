7a21cafed47d1a95de1e75bc2343835357c0142e
p-bo <pavel.borecki@gmail.com>
2018-12-26 16:08:51 +0000

    Translated using Weblate (Czech)
    Currently translated at 100,0% (68 of 68 strings)
    
    Translation: LXQt/LXQt Session (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-session/cs/

--- lxqt-config-session/translations/lxqt-config-session_cs.ts
+++ lxqt-config-session/translations/lxqt-config-session_cs.ts
@@ -125,12 +125,12 @@
     <message>
         <location filename="../basicsettings.ui" line="131"/>
         <source>Suspend/hibernate after lock delay:</source>
-        <translation type="unfinished"></translation>
+        <translation>Uspat/hibernovat pokud uzamčeno déle než:</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="144"/>
         <source> ms</source>
-        <translation type="unfinished"></translation>
+        <translation> ms</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="23"/>
