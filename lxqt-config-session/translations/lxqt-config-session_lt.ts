a704a4de2c2077069c6f11abd5a885353005ba3b
Moo <hazap@hotmail.com>
2019-01-01 14:06:07 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (68 of 68 strings)
    
    Translation: LXQt/LXQt Session (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-session/lt/

--- lxqt-config-session/translations/lxqt-config-session_lt.ts
+++ lxqt-config-session/translations/lxqt-config-session_lt.ts
@@ -362,7 +362,7 @@
     <message>
         <location filename="../userlocationspage.cpp" line="78"/>
         <source>Default folder to load or save music from or to</source>
-        <translation type="unfinished"></translation>
+        <translation>Numatytasis aplankas iš kurio įkelti arba į kurį įrašyti muziką</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="79"/>
