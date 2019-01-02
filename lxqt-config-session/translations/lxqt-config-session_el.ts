8663cb41099b63c68fc89c30029c522d069da50c
Dimitrios Glentadakis <dglent@free.fr>
2019-01-02 05:09:11 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (68 of 68 strings)
    
    Translation: LXQt/LXQt Session (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-session/el/

--- lxqt-config-session/translations/lxqt-config-session_el.ts
+++ lxqt-config-session/translations/lxqt-config-session_el.ts
@@ -362,7 +362,7 @@
     <message>
         <location filename="../userlocationspage.cpp" line="78"/>
         <source>Default folder to load or save music from or to</source>
-        <translation type="unfinished"></translation>
+        <translation>Ο εξ ορισμού φάκελος φόρτωσης ή αποθήκευσης της μουσικής</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="79"/>
