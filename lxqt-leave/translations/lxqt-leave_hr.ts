af13d9f34ce9a412d3485aec01fb93c6636f2ced
anotherus3r <teoo987@protonmail.com>
2019-01-30 17:21:26 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Session (Leave)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-leave/hr/

--- lxqt-leave/translations/lxqt-leave_hr.ts
+++ lxqt-leave/translations/lxqt-leave_hr.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="../leavedialog.ui" line="20"/>
         <source>Leave</source>
-        <translation type="unfinished">Napusti</translation>
+        <translation>Napusti</translation>
     </message>
     <message>
         <location filename="../leavedialog.ui" line="71"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Što želite da računalo učini?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Što želite da računalo učini?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="51"/>
         <source>Logout</source>
-        <translation type="unfinished">Odjava</translation>
+        <translation>Odjava</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="69"/>
         <source>Reboot</source>
-        <translation type="unfinished">Ponovno podizanje</translation>
+        <translation>Ponovno pokretanje</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="56"/>
         <source>Shutdown</source>
-        <translation type="unfinished">Isključivanje</translation>
+        <translation>Isključivanje</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="61"/>
         <source>Suspend</source>
-        <translation type="unfinished">suspenzija</translation>
+        <translation>Suspenzija</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="66"/>
         <source>Lock screen</source>
-        <translation type="unfinished">Zaključavanje zaslona</translation>
+        <translation>Zaključavanje zaslona</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="74"/>
         <source>Hibernate</source>
-        <translation type="unfinished">Hibernacija</translation>
+        <translation>Hibernacija</translation>
     </message>
     <message>
         <location filename="../leavedialog.ui" line="51"/>
         <source>Cancel</source>
-        <translation type="unfinished">Otkaži</translation>
+        <translation>Otkaži</translation>
     </message>
 </context>
 <context>
@@ -54,32 +54,32 @@
     <message>
         <location filename="../main.cpp" line="58"/>
         <source>Logout.</source>
-        <translation type="unfinished">Odjava.</translation>
+        <translation>Odjava.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="61"/>
         <source>Lockscreen.</source>
-        <translation type="unfinished">Zaključaj zaslon.</translation>
+        <translation>Zaslon zaključavanja.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="64"/>
         <source>Suspend.</source>
-        <translation type="unfinished">Suspendiraj.</translation>
+        <translation>Suspendiraj.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="67"/>
         <source>Hibernate.</source>
-        <translation type="unfinished">Hibernacija.</translation>
+        <translation>Hibernacija.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="70"/>
         <source>Shutdown.</source>
-        <translation type="unfinished">Isključi.</translation>
+        <translation>Isključi.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="73"/>
         <source>Reboot.</source>
-        <translation type="unfinished">Ponovno podigni.</translation>
+        <translation>Ponovno pokreni.</translation>
     </message>
 </context>
 </TS>
