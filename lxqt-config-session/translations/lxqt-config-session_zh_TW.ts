4daed4024a2da8cad789ba42cbe4d650b2c98f8a
BavonChao <chaochinghua@outlook.com>
2019-06-12 08:48:10 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (68 of 68 strings)
    
    Translation: LXQt/LXQt Session (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-session/zh_TW/

--- lxqt-config-session/translations/lxqt-config-session_zh_TW.ts
+++ lxqt-config-session/translations/lxqt-config-session_zh_TW.ts
@@ -125,12 +125,12 @@
     <message>
         <location filename="../basicsettings.ui" line="131"/>
         <source>Suspend/hibernate after lock delay:</source>
-        <translation type="unfinished"></translation>
+        <translation>鎖定延遲後掛起/休眠：</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="144"/>
         <source> ms</source>
-        <translation type="unfinished"></translation>
+        <translation> ms</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="23"/>
@@ -362,7 +362,7 @@
     <message>
         <location filename="../userlocationspage.cpp" line="78"/>
         <source>Default folder to load or save music from or to</source>
-        <translation type="unfinished"></translation>
+        <translation>讀取或保存音樂的默認文件夾</translation>
     </message>
     <message>
         <location filename="../userlocationspage.cpp" line="79"/>
