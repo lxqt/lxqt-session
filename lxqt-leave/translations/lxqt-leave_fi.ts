50181466da612bbf70feb70e7bf15f42978fe9d2
Chiitoo <escomk3@hotmail.com>
2019-10-27 16:02:25 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Session (Leave)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-leave/fi/

--- lxqt-leave/translations/lxqt-leave_fi.ts
+++ lxqt-leave/translations/lxqt-leave_fi.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="../leavedialog.ui" line="20"/>
         <source>Leave</source>
-        <translation type="unfinished"></translation>
+        <translation>Poistu</translation>
     </message>
     <message>
         <location filename="../leavedialog.ui" line="71"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Mitä haluaisit tietokoneen tekevän?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="51"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Kirjaudu Ulos</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="69"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Käynnistä Uudelleen</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="56"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>Sammuta</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="61"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Lepotila</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="66"/>
         <source>Lock screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Lukitse Näyttö</translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="74"/>
         <source>Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Horrostila</translation>
     </message>
     <message>
         <location filename="../leavedialog.ui" line="51"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Peru</translation>
     </message>
 </context>
 <context>
@@ -54,32 +54,32 @@
     <message>
         <location filename="../main.cpp" line="58"/>
         <source>Logout.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kirjaudu ulos.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="61"/>
         <source>Lockscreen.</source>
-        <translation type="unfinished"></translation>
+        <translation>Lukitse näyttö.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="64"/>
         <source>Suspend.</source>
-        <translation type="unfinished"></translation>
+        <translation>Siirry lepotilaan.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="67"/>
         <source>Hibernate.</source>
-        <translation type="unfinished"></translation>
+        <translation>Siirry horrostilaan.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="70"/>
         <source>Shutdown.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sammuta tietokone.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="73"/>
         <source>Reboot.</source>
-        <translation type="unfinished"></translation>
+        <translation>Käynnistä tietokone uudelleen.</translation>
     </message>
 </context>
 </TS>
