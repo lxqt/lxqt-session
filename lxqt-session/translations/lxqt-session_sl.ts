065df8a50214ba2f26243488ac0ff68f88128ab3
Nejc Bertoncelj <nbert@siol.net>
2020-01-26 17:39:50 +0000

    Translated using Weblate (Slovenian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: LXQt/LXQt Session
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-session/sl/

--- lxqt-session/translations/lxqt-session_sl.ts
+++ lxqt-session/translations/lxqt-session_sl.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/lxqtmodman.cpp" line="311"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Poročilo o sesutju</translation>
     </message>
     <message>
         <location filename="../src/lxqtmodman.cpp" line="312"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; se je prevečkrat sesul. Do nadalnje prijave bo onemogočen.</translation>
     </message>
 </context>
 <context>
@@ -19,18 +19,18 @@
     <message>
         <location filename="../src/main.cpp" line="62"/>
         <source>Configuration file path.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pot do konfiguracijskih datotek.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="62"/>
         <location filename="../src/main.cpp" line="63"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>datoteka</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="63"/>
         <source>Window manager to use.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uporabljen upravljalnik oken.</translation>
     </message>
 </context>
 <context>
@@ -53,12 +53,12 @@
     <message>
         <location filename="../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Dobrodošli v LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Prosimo, izberite privzetega upravljalnika oken.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="71"/>
         <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Lahko ga spremenite kadarkoli prek menijev Možnosti -&gt; Nastavitve LXQt -&gt; Nastavitev seje.</translation>
     </message>
 </context>
 </TS>
