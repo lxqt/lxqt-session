586a3a964eade15f290247eae0da7eaf263b22fe
Mincho Kondarev <mkondarev@yahoo.de>
2020-12-07 18:29:26 +0100

    Weblate commits
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/bg/
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-session

---
+++ lxqt-session/translations/lxqt-session_bg.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>LXQtModuleManager</name>
+    <message>
+        <location filename="../src/lxqtmodman.cpp" line="311"/>
+        <source>Crash Report</source>
+        <translation>Доклад за срив</translation>
+    </message>
+    <message>
+        <location filename="../src/lxqtmodman.cpp" line="312"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; се срина твърде много пъти. Автоматичното му рестартиране е деактивирано до следващото влизане.</translation>
+    </message>
+</context>
+<context>
+    <name>SessionApplication</name>
+    <message>
+        <location filename="../src/main.cpp" line="62"/>
+        <source>Configuration file path.</source>
+        <translation>Път на конфигурационен файл.</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="62"/>
+        <location filename="../src/main.cpp" line="63"/>
+        <source>file</source>
+        <translation>файл</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="63"/>
+        <source>Window manager to use.</source>
+        <translation>Мениджър на прозорци, който ще се използва.</translation>
+    </message>
+</context>
+<context>
+    <name>WmSelectDialog</name>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="14"/>
+        <source>Welcome to LXQt</source>
+        <translation>Добре дошли в LXQt</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="20"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Добре дошли в LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Моля изберете мениджър на прозорци.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="71"/>
+        <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
+        <translation>Ще можете да промените това по всяко време чрез Системни настройки -&gt; Конфигурационен център LXQt -&gt; Настройки на сесията -&gt; Основни настройки.</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.cpp" line="65"/>
+        <source>Other ...</source>
+        <translation>Друго ...</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.cpp" line="66"/>
+        <source>Choose your favorite one.</source>
+        <translation>Изберете вашето предпочитание.</translation>
+    </message>
+</context>
+</TS>
