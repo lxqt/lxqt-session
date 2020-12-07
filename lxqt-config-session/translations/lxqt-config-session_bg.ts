de6768c3917eee38d1d86ceedd1c06659b32eb2f
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2020-12-07 19:03:46 +0100

    Weblate commits (#346)
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/bg/
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-session
    
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>

---
+++ lxqt-config-session/translations/lxqt-config-session_bg.ts
@@ -0,0 +1,336 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>AutoStartEdit</name>
+    <message>
+        <location filename="../autostartedit.ui" line="14"/>
+        <source>Edit</source>
+        <translation>Редактиране</translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.ui" line="20"/>
+        <source>Name:</source>
+        <translation>Име:</translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.ui" line="27"/>
+        <source>Search...</source>
+        <translation>Търсене...</translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.ui" line="47"/>
+        <source>Command:</source>
+        <translation>Команда:</translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.ui" line="57"/>
+        <source>Wait for system tray</source>
+        <translation>Изчакване на системната област за известяване</translation>
+    </message>
+    <message>
+        <location filename="../autostartedit.cpp" line="61"/>
+        <source>Select Application</source>
+        <translation>Избиране на приложение</translation>
+    </message>
+</context>
+<context>
+    <name>AutoStartItemModel</name>
+    <message>
+        <location filename="../autostartmodel.cpp" line="193"/>
+        <source>Global Autostart</source>
+        <translation>Глобално автоматично стартиращи програми</translation>
+    </message>
+    <message>
+        <location filename="../autostartmodel.cpp" line="195"/>
+        <source>LXQt Autostart</source>
+        <translation>LXQt автоматично стартиращи програми</translation>
+    </message>
+    <message>
+        <location filename="../autostartmodel.cpp" line="213"/>
+        <source>Location: %1</source>
+        <translation>Местоположение: %1</translation>
+    </message>
+    <message>
+        <location filename="../autostartmodel.cpp" line="215"/>
+        <source>Overrides: %1</source>
+        <translation>Преопределяне: %1</translation>
+    </message>
+</context>
+<context>
+    <name>AutoStartPage</name>
+    <message>
+        <location filename="../autostartpage.ui" line="17"/>
+        <source>Delete</source>
+        <translation>Изтриване</translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.ui" line="24"/>
+        <source>Add</source>
+        <translation>Добавяне</translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.ui" line="31"/>
+        <source>Edit</source>
+        <translation>Редактиране</translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.ui" line="44"/>
+        <source>Application Autostart</source>
+        <translation>Приложения</translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.cpp" line="143"/>
+        <source>Error</source>
+        <translation>Грешка</translation>
+    </message>
+    <message>
+        <location filename="../autostartpage.cpp" line="143"/>
+        <source>File &apos;%1&apos; already exists!</source>
+        <translation>Файл &apos;%1&apos; вече съществува!</translation>
+    </message>
+</context>
+<context>
+    <name>BasicSettings</name>
+    <message>
+        <location filename="../basicsettings.ui" line="23"/>
+        <source>Basic Settings</source>
+        <translation>Основни настройки</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="30"/>
+        <source>Window Manager</source>
+        <translation>Мениджър на прозорци</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="49"/>
+        <source>Search...</source>
+        <translation>Търсене...</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="59"/>
+        <source>LXQt Modules</source>
+        <translation>LXQt Модули</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="65"/>
+        <source>Start</source>
+        <translation>Старт</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="72"/>
+        <source>Stop</source>
+        <translation>Стоп</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="111"/>
+        <source>Leave Session</source>
+        <translation>Излизане от сесията</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="117"/>
+        <source>Ask for confirmation to leave session</source>
+        <translation>Изискване на потвърждение за напускане на сесия</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="124"/>
+        <source>Lock screen before suspending/hibernating</source>
+        <translation>Заключване на екрана преди приспиване/хибернация</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="131"/>
+        <source>Suspend/hibernate after lock delay:</source>
+        <translation>При приспиване и хибернация заключване след:</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.ui" line="144"/>
+        <source> ms</source>
+        <translation> ms</translation>
+    </message>
+    <message>
+        <location filename="../basicsettings.cpp" line="156"/>
+        <source>Select a window manager</source>
+        <translation>Избиране на мениджър на прозорци</translation>
+    </message>
+</context>
+<context>
+    <name>EnvironmentPage</name>
+    <message>
+        <location filename="../environmentpage.ui" line="17"/>
+        <source>Add</source>
+        <translation>Добавяне</translation>
+    </message>
+    <message>
+        <location filename="../environmentpage.ui" line="24"/>
+        <source>Delete</source>
+        <translation>Изтриване</translation>
+    </message>
+    <message>
+        <location filename="../environmentpage.ui" line="50"/>
+        <source>Environment Variables (Advanced)</source>
+        <translation>Променливи на средата (разширени настройки)</translation>
+    </message>
+    <message>
+        <location filename="../environmentpage.ui" line="61"/>
+        <source>Variable Name</source>
+        <translation>Име на променлива</translation>
+    </message>
+    <message>
+        <location filename="../environmentpage.ui" line="66"/>
+        <source>Value</source>
+        <translation>Стойност</translation>
+    </message>
+</context>
+<context>
+    <name>ModuleModel</name>
+    <message>
+        <location filename="../modulemodel.cpp" line="87"/>
+        <source>Running</source>
+        <translation>Стартиран</translation>
+    </message>
+</context>
+<context>
+    <name>SessionConfigWindow</name>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="44"/>
+        <source>LXQt Session Settings</source>
+        <translation>Настройки на сесията</translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="47"/>
+        <source>Basic Settings</source>
+        <translation>Основни настройки</translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="53"/>
+        <source>User Directories</source>
+        <translation>Потребителски директории</translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="59"/>
+        <source>Autostart</source>
+        <translation>Автоматично стартиране на програми</translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="65"/>
+        <source>Environment (Advanced)</source>
+        <translation>Променливи на средата(разширени настройки)</translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="87"/>
+        <source>Session Restart Required</source>
+        <translation>Необходимо е рестартиране на сесията</translation>
+    </message>
+    <message>
+        <location filename="../sessionconfigwindow.cpp" line="88"/>
+        <source>Some settings will not take effect until the next log in.</source>
+        <translation>Някои настройки няма да влязат в сила до следващото влизане.</translation>
+    </message>
+</context>
+<context>
+    <name>UserLocationsPage</name>
+    <message>
+        <location filename="../userlocationspage.cpp" line="116"/>
+        <source>Locations for Personal Files</source>
+        <translation>Местоположения за персоналните файлове</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="185"/>
+        <source>LXQt Session Settings - User Directories</source>
+        <translation>Настройки на сесията LXQt - Потребителски директории</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="186"/>
+        <source>An error ocurred while applying the settings for the %1 location</source>
+        <translation>Възникна грешка при прилагане на настройките за местоположение %1</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="202"/>
+        <source>Choose Location</source>
+        <translation>Избиране на местоположение</translation>
+    </message>
+</context>
+<context>
+    <name>UserLocationsPrivate</name>
+    <message>
+        <location filename="../userlocationspage.cpp" line="63"/>
+        <source>Desktop</source>
+        <translation>Работен плот</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="64"/>
+        <source>Downloads</source>
+        <translation>Свалени</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="65"/>
+        <source>Templates</source>
+        <translation>Шаблони</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="66"/>
+        <source>Public Share</source>
+        <translation>Споделени</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="67"/>
+        <source>Documents</source>
+        <translation>Документи</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="68"/>
+        <source>Music</source>
+        <translation>Музика</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="69"/>
+        <source>Pictures</source>
+        <translation>Снимки</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="70"/>
+        <source>Videos</source>
+        <translation>Видео</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="73"/>
+        <source>Contains all the files which you see on your desktop</source>
+        <translation>Съдържа всички файлове, които виждате на вашия работен плот</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="74"/>
+        <source>Default folder to save your downloaded files</source>
+        <translation>Папка по подразбиране за свалените файлове</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="75"/>
+        <source>Default folder to load or save templates from or to</source>
+        <translation>Папка по подразбиране за шаблоните</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="76"/>
+        <source>Default folder to publicly share your files</source>
+        <translation>Папка по подразбиране за споделените файлове</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="77"/>
+        <source>Default folder to load or save documents from or to</source>
+        <translation>Папка по подразбиране за документи</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="78"/>
+        <source>Default folder to load or save music from or to</source>
+        <translation>Папка по подразбиране за съхраняване на музика</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="79"/>
+        <source>Default folder to load or save pictures from or to</source>
+        <translation>Папка по подразбиране за снимки и изображения</translation>
+    </message>
+    <message>
+        <location filename="../userlocationspage.cpp" line="80"/>
+        <source>Default folder to load or save videos from or to</source>
+        <translation>Папка по подразбиране за съхраняване на видео</translation>
+    </message>
+</context>
+</TS>
