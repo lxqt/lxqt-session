27d11cd48fb589549a30160b83aae08791c7036d
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-03-07 13:31:44 +0100

    Translations update from Weblate (#369)
    * Weblate commits
    
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/ja/
    Translation: LXQt Configuration/lxqt-session
    
    * Weblate commits
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    
    * Weblate commits
    
    Co-authored-by: Baptiste Huchon <baptiste.huchon@orange.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-suspend/fr/
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Suspend
    
    * Weblate commits
    
    Co-authored-by: Pavel Fric <pavelfric@yahoo.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/cs/
    Translation: LXQt Configuration/lxqt-config-session
    
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Co-authored-by: helabasa <R45XvezA@pm.me>
    Co-authored-by: Baptiste Huchon <baptiste.huchon@orange.fr>
    Co-authored-by: Pavel Fric <pavelfric@yahoo.com>

--- lxqt-leave/translations/lxqt-leave_si.ts
+++ lxqt-leave/translations/lxqt-leave_si.ts
@@ -1,52 +1,52 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr">
+<TS version="2.1" language="si">
 <context>
     <name>LeaveDialog</name>
     <message>
         <location filename="../leavedialog.ui" line="20"/>
         <source>Leave</source>
-        <translation>Quitter</translation>
-    </message>
-    <message>
-        <location filename="../leavedialog.ui" line="51"/>
-        <source>Cancel</source>
-        <translation>Annuler</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../leavedialog.ui" line="71"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Que voulez-vous que l&apos;ordinateur fasse ?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="55"/>
         <source>Logout</source>
-        <translation>Déconnexion</translation>
-    </message>
-    <message>
-        <location filename="../leavedialog.cpp" line="70"/>
-        <source>Lock screen</source>
-        <translation>Verrouiller l&apos;écran</translation>
-    </message>
-    <message>
-        <location filename="../leavedialog.cpp" line="65"/>
-        <source>Suspend</source>
-        <translation>Mettre en veille</translation>
-    </message>
-    <message>
-        <location filename="../leavedialog.cpp" line="78"/>
-        <source>Hibernate</source>
-        <translation>Hiberner</translation>
-    </message>
-    <message>
-        <location filename="../leavedialog.cpp" line="60"/>
-        <source>Shutdown</source>
-        <translation>Arrêter</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../leavedialog.cpp" line="73"/>
         <source>Reboot</source>
-        <translation>Redémarrer</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="60"/>
+        <source>Shutdown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="65"/>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="70"/>
+        <source>Lock screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="78"/>
+        <source>Hibernate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="51"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -54,32 +54,32 @@
     <message>
         <location filename="../main.cpp" line="60"/>
         <source>Logout.</source>
-        <translation>Déconnexion.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.cpp" line="63"/>
         <source>Lockscreen.</source>
-        <translation>Verrouillage de l&apos;écran.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.cpp" line="66"/>
         <source>Suspend.</source>
-        <translation>Mettre en veille.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.cpp" line="69"/>
         <source>Hibernate.</source>
-        <translation>Hibernation.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.cpp" line="72"/>
         <source>Shutdown.</source>
-        <translation>Arrêt.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.cpp" line="75"/>
         <source>Reboot.</source>
-        <translation>Redémarrage.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
