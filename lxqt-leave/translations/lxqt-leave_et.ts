a0a2632a70cff850a20c4a1e60528174e944e6aa
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-27 22:07:22 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-hibernate/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-leave-dialog/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-lock-screen/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-logout/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-shutdown/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-suspend/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/xscreensaver-module-name/et/
    Translation: LXQt Configuration/XScreenSaver Autostart Name
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-config-session Menu Entry
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Hibernate
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Leave Dialog
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Lock Screen
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Logout
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Shutdown
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Suspend
    Translation: LXQt Configuration/lxqt-session

---
+++ lxqt-leave/translations/lxqt-leave_et.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Lahku</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="71"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Mida sa soovid praegu arvutiga teha?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="55"/>
+        <source>Logout</source>
+        <translation>Logi välja</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="73"/>
+        <source>Reboot</source>
+        <translation>Taaskäivita</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="60"/>
+        <source>Shutdown</source>
+        <translation>Seiska</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="65"/>
+        <source>Suspend</source>
+        <translation>Vaju unne</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="70"/>
+        <source>Lock screen</source>
+        <translation>Lukusta ekraan</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="78"/>
+        <source>Hibernate</source>
+        <translation>Vaju sügavasse unne</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="51"/>
+        <source>Cancel</source>
+        <translation>Tühista</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Logout.</source>
+        <translation>Logi välja.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="63"/>
+        <source>Lockscreen.</source>
+        <translation>Lukusta ekraan.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="66"/>
+        <source>Suspend.</source>
+        <translation>Vaju unne.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>Hibernate.</source>
+        <translation>Vaju sügavasse unne.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="72"/>
+        <source>Shutdown.</source>
+        <translation>Lülita välja.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="75"/>
+        <source>Reboot.</source>
+        <translation>Taaskäivita arvuti.</translation>
+    </message>
+</context>
+</TS>
