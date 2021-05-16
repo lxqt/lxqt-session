28974db9e13e9ae54ba5b8fa76bf824c942f8543
dimuaconheo <dimuaconheo@gmail.com>
2021-05-16 17:07:28 +0200

    Weblate commits
    Co-authored-by: dimuaconheo <dimuaconheo@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session-menu-entry/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-hibernate/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-leave-dialog/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-lock-screen/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-logout/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-reboot/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-shutdown/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-suspend/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/xscreensaver-module-name/vi/
    Translation: LXQt Configuration/XScreenSaver Autostart Name
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-config-session Menu Entry
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Hibernate
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Leave Dialog
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Lock Screen
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Logout
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Reboot
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Shutdown
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Suspend

---
+++ lxqt-leave/translations/lxqt-leave_vi.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Rời</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="71"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Bạn muốn máy tính làm gì?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="55"/>
+        <source>Logout</source>
+        <translation>Đăng xuất</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="73"/>
+        <source>Reboot</source>
+        <translation>Khởi động lại</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="60"/>
+        <source>Shutdown</source>
+        <translation>Tăt máy</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="65"/>
+        <source>Suspend</source>
+        <translation>Tạm ngưng</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="70"/>
+        <source>Lock screen</source>
+        <translation>Khóa màn hình</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="78"/>
+        <source>Hibernate</source>
+        <translation>Ngủ đông</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="51"/>
+        <source>Cancel</source>
+        <translation>Hủy</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Logout.</source>
+        <translation>Đăng xuất.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="63"/>
+        <source>Lockscreen.</source>
+        <translation>Màn hình khóa.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="66"/>
+        <source>Suspend.</source>
+        <translation>Tạm ngưng.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>Hibernate.</source>
+        <translation>Ngủ đông.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="72"/>
+        <source>Shutdown.</source>
+        <translation>Tắt máy.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="75"/>
+        <source>Reboot.</source>
+        <translation>Khởi động lại.</translation>
+    </message>
+</context>
+</TS>
