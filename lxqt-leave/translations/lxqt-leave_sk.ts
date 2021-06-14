cf26f9919e354a08336ed980c5b88aa4867c2de8
pavol polonec <pavolpolonec@gmail.com>
2021-06-14 14:23:54 +0200

    Weblate commits
    Co-authored-by: pavol polonec <pavolpolonec@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/sk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-hibernate/sk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-leave-dialog/sk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-logout/sk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-reboot/sk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-suspend/sk/
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Hibernate
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Leave Dialog
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Logout
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Reboot
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Suspend

---
+++ lxqt-leave/translations/lxqt-leave_sk.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Odísť</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="71"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Čo má počítač urobiť?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="55"/>
+        <source>Logout</source>
+        <translation>Odhlásiť ma</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="73"/>
+        <source>Reboot</source>
+        <translation>Reštartovať</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="60"/>
+        <source>Shutdown</source>
+        <translation>Vypnúť</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="65"/>
+        <source>Suspend</source>
+        <translation>Uspať</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="70"/>
+        <source>Lock screen</source>
+        <translation>Uzamknúť obrazovku</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="78"/>
+        <source>Hibernate</source>
+        <translation>Hibernovať</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="51"/>
+        <source>Cancel</source>
+        <translation>Zrušiť</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Logout.</source>
+        <translation>Odhlásiť používateľa.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="63"/>
+        <source>Lockscreen.</source>
+        <translation>Zamknúť obrazovku.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="66"/>
+        <source>Suspend.</source>
+        <translation>Uspať.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>Hibernate.</source>
+        <translation>Hibernovať.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="72"/>
+        <source>Shutdown.</source>
+        <translation>Vypnúť.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="75"/>
+        <source>Reboot.</source>
+        <translation>Reštartovať.</translation>
+    </message>
+</context>
+</TS>
