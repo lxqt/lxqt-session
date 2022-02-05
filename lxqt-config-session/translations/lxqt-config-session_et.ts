ddff9f39de8d3aab37feb42e28bd343dcaa01d58
Weblate <noreply@weblate.org>
2022-02-05 21:08:56 +0100

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/et/
    Translation: LXQt Configuration/lxqt-config-session

--- lxqt-config-session/translations/lxqt-config-session_et.ts
+++ lxqt-config-session/translations/lxqt-config-session_et.ts
@@ -125,12 +125,12 @@
     <message>
         <location filename="../basicsettings.ui" line="111"/>
         <source>Global Screen Scaling</source>
-        <translation type="unfinished"></translation>
+        <translation>Keskkonnaülene ekraani skaleerimine</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="123"/>
         <source>Scale factor:</source>
-        <translation type="unfinished"></translation>
+        <translation>Skaleerimisfaktor:</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="149"/>
