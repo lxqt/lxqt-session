7dd87070b7c27e55d7d18bef7b652af21662e97c
Weblate <noreply@weblate.org>
2022-03-29 10:48:07 +0200

    Weblate commits
    Co-authored-by: pavol polonec <pavolpolonec@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/sk/
    Translation: LXQt Configuration/lxqt-config-session

--- lxqt-config-session/translations/lxqt-config-session_sk_SK.ts
+++ lxqt-config-session/translations/lxqt-config-session_sk_SK.ts
@@ -120,12 +120,12 @@
     <message>
         <location filename="../basicsettings.ui" line="111"/>
         <source>Global Screen Scaling</source>
-        <translation type="unfinished"></translation>
+        <translation>Globálne meradlo zobrazenia</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="123"/>
         <source>Scale factor:</source>
-        <translation type="unfinished"></translation>
+        <translation>Násobok meradla:</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="149"/>
