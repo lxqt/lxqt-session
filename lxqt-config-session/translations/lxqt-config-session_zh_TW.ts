7f1263e56bf4d9a2ab6b2cbea65aafb1663a6474
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-07-08 17:01:12 +0200

    Weblate commits (#442)
    Co-authored-by: NightFeather <ss900328@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/zh_TW/
    Translation: LXQt Configuration/lxqt-config-session

--- lxqt-config-session/translations/lxqt-config-session_zh_TW.ts
+++ lxqt-config-session/translations/lxqt-config-session_zh_TW.ts
@@ -110,12 +110,12 @@
     <message>
         <location filename="../basicsettings.ui" line="111"/>
         <source>Global Screen Scaling</source>
-        <translation type="unfinished"></translation>
+        <translation>全域畫面縮放</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="123"/>
         <source>Scale factor:</source>
-        <translation type="unfinished"></translation>
+        <translation>縮放比例：</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="149"/>
