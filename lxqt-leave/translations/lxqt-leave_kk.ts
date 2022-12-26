128a06df1f0b8d38239184f10d425ab7f11ff4e0
Weblate <noreply@weblate.org>
2022-12-26 23:10:53 +0100

    Weblate commits
    Co-authored-by: Baurzhan Muftakhidinov <baurthefirst@gmail.com>
    Co-authored-by: Carl Andersen <carl@flying-cloud.dk>
    Co-authored-by: Stribor <gejmermi@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-hibernate/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-leave-dialog/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-logout/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-reboot/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-suspend/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/sr/
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Hibernate
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Leave Dialog
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Logout
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Reboot
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Suspend
    Translation: LXQt Configuration/lxqt-session

---
+++ lxqt-leave/translations/lxqt-leave_kk.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kk">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Шығу</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="71"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Компьютердің не істегенін қалайсыз?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="55"/>
+        <source>Logout</source>
+        <translation>Шығу</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="73"/>
+        <source>Reboot</source>
+        <translation>Қайта жүктеу</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="60"/>
+        <source>Shutdown</source>
+        <translation>Сөндіру</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="65"/>
+        <source>Suspend</source>
+        <translation>Ұйықтату</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="70"/>
+        <source>Lock screen</source>
+        <translation>Экранды құлыптау</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="78"/>
+        <source>Hibernate</source>
+        <translation>Гибернациялау</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="51"/>
+        <source>Cancel</source>
+        <translation>Бас тарту</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Logout.</source>
+        <translation>Шығу.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="63"/>
+        <source>Lockscreen.</source>
+        <translation>Экранды құлыптау.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="66"/>
+        <source>Suspend.</source>
+        <translation>Ұйықтату.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>Hibernate.</source>
+        <translation>Гибернациялау.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="72"/>
+        <source>Shutdown.</source>
+        <translation>Сөндіру.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="75"/>
+        <source>Reboot.</source>
+        <translation>Қайта жүктеу.</translation>
+    </message>
+</context>
+</TS>
