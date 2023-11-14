732e97348aa3212b836f45456d11210a3baca158
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-11-14 14:46:18 +0100

    Weblate commits (#483)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/hr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/sk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/uk/
    Translation: LXQt Configuration/lxqt-session
    
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Jalakas <anari.jalakas@gmail.com>
    Co-authored-by: Paolo Vigoroso <xpaolox@zoznam.sk>
    Co-authored-by: Xavier Paniello <xavip@medusaweb.cat>
    Co-authored-by: milotype <mail@milotype.de>

--- lxqt-session/translations/lxqt-session_et.ts
+++ lxqt-session/translations/lxqt-session_et.ts
@@ -35,12 +35,12 @@
     <message>
         <location filename="../src/sessionapplication.cpp" line="83"/>
         <source>DBus Environment</source>
-        <translation type="unfinished"></translation>
+        <translation>DBus keskkkond</translation>
     </message>
     <message>
         <location filename="../src/sessionapplication.cpp" line="84"/>
         <source>The DBus Activation Environment wasn&apos;t updated. Some apps might not work properly</source>
-        <translation type="unfinished"></translation>
+        <translation>DBusi aktiveerimiskeskkonda ei värskendatud. Mõned rakendused ei pruugi korralikult töötada</translation>
     </message>
 </context>
 <context>
--- lxqt-session/translations/lxqt-session_pt.ts
+++ lxqt-session/translations/lxqt-session_pt.ts
@@ -35,12 +35,12 @@
     <message>
         <location filename="../src/sessionapplication.cpp" line="83"/>
         <source>DBus Environment</source>
-        <translation type="unfinished"></translation>
+        <translation>Ambiente DBus</translation>
     </message>
     <message>
         <location filename="../src/sessionapplication.cpp" line="84"/>
         <source>The DBus Activation Environment wasn&apos;t updated. Some apps might not work properly</source>
-        <translation type="unfinished"></translation>
+        <translation>O ambiente de ativação do DBus não foi atualizado. Algumas aplicações poderão não funcionar corretamente</translation>
     </message>
 </context>
 <context>
