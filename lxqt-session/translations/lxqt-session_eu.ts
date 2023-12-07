068d6572c3d07f17ee067371a450329d9a609be2
Weblate <noreply@weblate.org>
2023-12-07 23:23:27 +0100

    Weblate commits
    Co-authored-by: Gabriel Alejandro López López <glpz@daxslab.com>
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Roberalz <lumintnian@outlook.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/es/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/eu/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/fi/
    Translation: LXQt Configuration/lxqt-session

--- lxqt-session/translations/lxqt-session_eu.ts
+++ lxqt-session/translations/lxqt-session_eu.ts
@@ -35,12 +35,12 @@
     <message>
         <location filename="../src/sessionapplication.cpp" line="83"/>
         <source>DBus Environment</source>
-        <translation type="unfinished"></translation>
+        <translation>DBus ingurunea</translation>
     </message>
     <message>
         <location filename="../src/sessionapplication.cpp" line="84"/>
         <source>The DBus Activation Environment wasn&apos;t updated. Some apps might not work properly</source>
-        <translation type="unfinished"></translation>
+        <translation>DBus aktibatzeko ingurunea ez da eguneratu.  Baliteke aplikazio batzuk behar bezala ez funtzionatzea</translation>
     </message>
 </context>
 <context>
