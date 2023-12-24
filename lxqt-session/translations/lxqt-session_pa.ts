8be9ef4e6be211c38825d5de7629749f1a191098
Weblate <noreply@weblate.org>
2023-12-24 20:23:06 +0100

    Weblate commits
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Co-authored-by: Roberalz <lumintnian@outlook.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: jiaxianhua <iosdevlog@iosdevlog.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/es/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/eu/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/zh_CN/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-leave-dialog/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-lock-screen/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-logout/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-reboot/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-shutdown/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/zh_CN/
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-config-session Menu Entry
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Leave Dialog
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Lock Screen
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Logout
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Reboot
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Shutdown
    Translation: LXQt Configuration/lxqt-session

--- lxqt-session/translations/lxqt-session_pa.ts
+++ lxqt-session/translations/lxqt-session_pa.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<TS version="2.1" language="pa">
 <context>
     <name>LXQtModuleManager</name>
     <message>
         <location filename="../src/lxqtmodman.cpp" line="323"/>
         <source>Crash Report</source>
-        <translation>崩溃报告</translation>
+        <translation>ਕਰੈਸ਼ ਰਿਪੋਰਟ</translation>
     </message>
     <message>
         <location filename="../src/lxqtmodman.cpp" line="324"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation>&lt;b&gt;%1&lt;/b&gt; 多次崩溃，直至下一次登录前都不会再自动重新启动该程序。</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -19,28 +19,28 @@
     <message>
         <location filename="../src/main.cpp" line="62"/>
         <source>Configuration file path.</source>
-        <translation>配置文件路径。</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="62"/>
         <location filename="../src/main.cpp" line="63"/>
         <source>file</source>
-        <translation>文件</translation>
+        <translation>ਫਾਇਲ</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="63"/>
         <source>Window manager to use.</source>
-        <translation>要使用的窗口管理器。</translation>
+        <translation>ਵਰਤਣ ਲਈ ਵਿੰਡੋ ਮੈਨੇਜਰ।</translation>
     </message>
     <message>
         <location filename="../src/sessionapplication.cpp" line="83"/>
         <source>DBus Environment</source>
-        <translation>DBus 环境</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/sessionapplication.cpp" line="84"/>
         <source>The DBus Activation Environment wasn&apos;t updated. Some apps might not work properly</source>
-        <translation>DBus 激活环境没有更新。某些应用程序可能无法正常工作</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -48,27 +48,27 @@
     <message>
         <location filename="../src/wmselectdialog.ui" line="14"/>
         <source>Welcome to LXQt</source>
-        <translation>欢迎使用 LXQt</translation>
-    </message>
-    <message>
-        <location filename="../src/wmselectdialog.cpp" line="65"/>
-        <source>Other ...</source>
-        <translation>其它...</translation>
-    </message>
-    <message>
-        <location filename="../src/wmselectdialog.cpp" line="66"/>
-        <source>Choose your favorite one.</source>
-        <translation>选择您最喜欢的一个。</translation>
+        <translation>LXQt ਲਈ ਜੀ ਆਇਆਂ ਨੂੰ</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;欢迎使用 LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;请选择您的默认窗口管理器。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="71"/>
         <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
-        <translation>您可以随时在 首选项 -&gt; 会话配置 -&gt; 基本设置 中更改此设置。</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.cpp" line="65"/>
+        <source>Other ...</source>
+        <translation>ਹੋਰ ...</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.cpp" line="66"/>
+        <source>Choose your favorite one.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
