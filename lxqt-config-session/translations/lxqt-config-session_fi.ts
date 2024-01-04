a7e61a3726758b5503b38083c43c897a3d4143be
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-01-04 17:26:49 +0100

    Weblate commits (#494)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/pa/
    Translation: LXQt Configuration/lxqt-config-session
    
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Moo <hazap@hotmail.com>

--- lxqt-config-session/translations/lxqt-config-session_fi.ts
+++ lxqt-config-session/translations/lxqt-config-session_fi.ts
@@ -91,7 +91,7 @@
         <location filename="../autostartpage.cpp" line="138"/>
         <location filename="../autostartpage.cpp" line="161"/>
         <source>Please provide Name and Command</source>
-        <translation type="unfinished"></translation>
+        <translation>Anna nimi ja komento</translation>
     </message>
     <message>
         <location filename="../autostartpage.cpp" line="147"/>
