07f213113902d99b96ec0c3f04ab335e5a5cc7d3
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-02-16 18:25:09 +0100

    Weblate commits (#499)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/lg/
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-session
    
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>

---
+++ lxqt-session/translations/lxqt-session_lg.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>LXQtModuleManager</name>
+    <message>
+        <location filename="../src/lxqtmodman.cpp" line="323"/>
+        <source>Crash Report</source>
+        <translation>Alipoota ku kutabuka</translation>
+    </message>
+    <message>
+        <location filename="../src/lxqtmodman.cpp" line="324"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; eyitiriza okutabuka. Okwetandika kusibidwa okutuusa lw&apos;olitandika olutuula olupya.</translation>
+    </message>
+</context>
+<context>
+    <name>SessionApplication</name>
+    <message>
+        <location filename="../src/main.cpp" line="62"/>
+        <source>Configuration file path.</source>
+        <translation>Ekkubo erigguka ku fayiro omuli enteekateeka.</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="62"/>
+        <location filename="../src/main.cpp" line="63"/>
+        <source>file</source>
+        <translation>fayiro</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="63"/>
+        <source>Window manager to use.</source>
+        <translation>Ekitekateekamadirisa ekiba kikozesebwa.</translation>
+    </message>
+    <message>
+        <location filename="../src/sessionapplication.cpp" line="83"/>
+        <source>DBus Environment</source>
+        <translation>Ebikwatagana ne DBus</translation>
+    </message>
+    <message>
+        <location filename="../src/sessionapplication.cpp" line="84"/>
+        <source>The DBus Activation Environment wasn&apos;t updated. Some apps might not work properly</source>
+        <translation>Enkyukakyuka mu bikwatagana ne DBus tebikazidwa. Puloguramu ezimu ziyinza butakola nga bwe ziteekwa</translation>
+    </message>
+</context>
+<context>
+    <name>WmSelectDialog</name>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="14"/>
+        <source>Welcome to LXQt</source>
+        <translation>LXQt ekwanirizza</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="20"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;LXQt ekwanirizza&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Londa ekiteekateekamadirisa kyo ekya bulijjo.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="71"/>
+        <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
+        <translation>Kino osobola okukikyusa wonna w&apos;oyagalalira bw&apos;ogenda mu Enteekateeka -&gt; Enteekateeka z&apos;entuula -&gt; Enteekateeka ez&apos;awamu.</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.cpp" line="65"/>
+        <source>Other ...</source>
+        <translation>Birala...</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.cpp" line="66"/>
+        <source>Choose your favorite one.</source>
+        <translation>Londa ky&apos;oyagala.</translation>
+    </message>
+</context>
+</TS>
