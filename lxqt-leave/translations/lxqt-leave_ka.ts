bfc3865dda7786eababe71755b6c4ae9a7e4a6ee
Weblate <noreply@weblate.org>
2025-01-02 13:25:20 +0100

    Weblate commits
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-hibernate/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-leave-dialog/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-logout/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-reboot/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-shutdown/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-suspend/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/xscreensaver-module-name/ka/
    Translation: LXQt Configuration/XScreenSaver Autostart Name
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-config-session Menu Entry
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Hibernate
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Leave Dialog
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Logout
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Reboot
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Shutdown
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Suspend
    Translation: LXQt Configuration/lxqt-session

---
+++ lxqt-leave/translations/lxqt-leave_ka.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>გასვლა</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="71"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;რა გნებავთ, კომპიუტერმა ქნას?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="61"/>
+        <source>Logout</source>
+        <translation>სესიიდან გასვლა</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="79"/>
+        <source>Reboot</source>
+        <translation>გადატვირთვა</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="66"/>
+        <source>Shutdown</source>
+        <translation>გამორთვა</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="71"/>
+        <source>Suspend</source>
+        <translation>შეჩერება</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="76"/>
+        <source>Lock screen</source>
+        <translation>ეკრანის ჩაკეტვა</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="84"/>
+        <source>Hibernate</source>
+        <translation>პროგრამული ძილი</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="51"/>
+        <source>Cancel</source>
+        <translation>გაუქმება</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="59"/>
+        <source>Logout.</source>
+        <translation>სესიიდან გასვლა.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="62"/>
+        <source>Lockscreen.</source>
+        <translation>ეკრანის ჩაკეტვა.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="65"/>
+        <source>Suspend.</source>
+        <translation>შეჩერება.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="68"/>
+        <source>Hibernate.</source>
+        <translation>პროგრამული ძილი.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="71"/>
+        <source>Shutdown.</source>
+        <translation>გამორთვა.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="74"/>
+        <source>Reboot.</source>
+        <translation>გადატვირთვა.</translation>
+    </message>
+</context>
+</TS>
