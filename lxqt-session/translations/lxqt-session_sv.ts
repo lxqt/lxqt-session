92da907dec5fe2f581fe0d66c2a4cdf4af977dc0
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-06-16 18:21:47 +0200

    Weblate commits (#593)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session-menu-entry/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-hibernate/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-leave-dialog/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-lock-screen/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-logout/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-reboot/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-shutdown/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-suspend/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/xscreensaver-module-name/sv/
    Translation: LXQt Configuration/XScreenSaver Autostart Name
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-config-session Menu Entry
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Hibernate
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Leave Dialog
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Lock Screen
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Logout
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Reboot
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Shutdown
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Suspend
    Translation: LXQt Configuration/lxqt-session
    
    Co-authored-by: Simon Lindén <simon_l91@hotmail.com>
    Co-authored-by: bittin <bittin@reimu.nl>

---
+++ lxqt-session/translations/lxqt-session_sv.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>LXQtModuleManager</name>
+    <message>
+        <location filename="../src/lxqtmodman.cpp" line="347"/>
+        <source>Crash Report</source>
+        <translation>Kraschrapport</translation>
+    </message>
+    <message>
+        <location filename="../src/lxqtmodman.cpp" line="348"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; har kraschat för många gånger. Dess automatiska uppstart har avaktiverats tills nästa inloggning.</translation>
+    </message>
+</context>
+<context>
+    <name>SessionApplication</name>
+    <message>
+        <location filename="../src/main.cpp" line="62"/>
+        <source>Configuration file path.</source>
+        <translation>Sökväg till konfigurationsfil.</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="62"/>
+        <location filename="../src/main.cpp" line="63"/>
+        <source>file</source>
+        <translation>fil</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="63"/>
+        <source>Window manager to use.</source>
+        <translation>Fönsterhanterare som skall användas.</translation>
+    </message>
+    <message>
+        <location filename="../src/sessionapplication.cpp" line="89"/>
+        <source>DBus Environment</source>
+        <translation>DBus miljö</translation>
+    </message>
+    <message>
+        <location filename="../src/sessionapplication.cpp" line="90"/>
+        <source>The DBus Activation Environment wasn&apos;t updated. Some apps might not work properly</source>
+        <translation>DBus aktiveringsmiljön blev inte uppdaterad. Vissa program kommer eventuellt inte att fungera</translation>
+    </message>
+</context>
+<context>
+    <name>WmSelectDialog</name>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="14"/>
+        <source>Welcome to LXQt</source>
+        <translation>Välkommen till LXQt</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="20"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Välkommen till LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Vänligen ställ in den fönsterhanterare du vill använda som standard.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="71"/>
+        <source>You will be able to change this at any time through Preferences -&gt; Session Settings -&gt; Basic Settings.</source>
+        <translation>Du kan ändra detta när som helst via Inställningar -&gt; Sessionsinställningar -&gt; Grundinställningar.</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.cpp" line="65"/>
+        <source>Other ...</source>
+        <translation>Annan ...</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.cpp" line="66"/>
+        <source>Choose your favorite one.</source>
+        <translation>Välj din favorit.</translation>
+    </message>
+</context>
+</TS>
