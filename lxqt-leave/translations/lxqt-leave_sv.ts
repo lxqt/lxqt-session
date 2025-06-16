1b83e655c345a1d61dcc32f06f474774d6901930
Weblate <noreply@weblate.org>
2025-06-16 17:18:11 +0200

    Weblate commits
    Co-authored-by: Simon Lindén <simon_l91@hotmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: bittin <bittin@reimu.nl>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-leave/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session-menu-entry/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-hibernate/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-leave-dialog/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-lock-screen/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-logout/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-reboot/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-shutdown/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-suspend/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/xscreensaver-module-name/sv/
    Translation: LXQt Configuration/XScreenSaver Autostart Name
    Translation: LXQt Configuration/lxqt-config-leave
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-config-session Menu Entry
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Hibernate
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Leave Dialog
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Lock Screen
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Logout
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Reboot
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Shutdown
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Suspend
    Translation: LXQt Configuration/lxqt-session

---
+++ lxqt-leave/translations/lxqt-leave_sv.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>LeaveDialog</name>
+    <message>
+        <location filename="../leavedialog.ui" line="20"/>
+        <source>Leave</source>
+        <translation>Avsluta</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="71"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;What do you want the computer to do?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Vad vill du att datorn ska göra?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="61"/>
+        <source>Logout</source>
+        <translation>Logga ut</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="79"/>
+        <source>Reboot</source>
+        <translation>Starta om</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="66"/>
+        <source>Shutdown</source>
+        <translation>Stänga av</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="71"/>
+        <source>Suspend</source>
+        <translation>Vänteläge</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="76"/>
+        <source>Lock screen</source>
+        <translation>Lås skärm</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.cpp" line="84"/>
+        <source>Hibernate</source>
+        <translation>Viloläge</translation>
+    </message>
+    <message>
+        <location filename="../leavedialog.ui" line="51"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="59"/>
+        <source>Logout.</source>
+        <translation>Logga ut.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="62"/>
+        <source>Lockscreen.</source>
+        <translation>Lås skärm.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="65"/>
+        <source>Suspend.</source>
+        <translation>Vänteläge.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="68"/>
+        <source>Hibernate.</source>
+        <translation>Viloläge.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="71"/>
+        <source>Shutdown.</source>
+        <translation>Stänga av.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="74"/>
+        <source>Reboot.</source>
+        <translation>Starta om.</translation>
+    </message>
+</context>
+</TS>
