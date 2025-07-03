c83952b7167444fe2f835a638ef186c1943721a3
Weblate <noreply@weblate.org>
2025-07-03 16:27:00 +0200

    Weblate commits
    Co-authored-by: BigELK176 ≡ <BigELK176@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session-menu-entry/zh_TW/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/zh_CN/
    Translation: LXQt Configuration/lxqt-config-session Menu Entry
    Translation: LXQt Configuration/lxqt-session

--- lxqt-session/translations/lxqt-session_zh_CN.ts
+++ lxqt-session/translations/lxqt-session_zh_CN.ts
@@ -53,7 +53,7 @@
     <message>
         <location filename="../src/wmselectdialog.cpp" line="65"/>
         <source>Other ...</source>
-        <translation>其它...</translation>
+        <translation>其他...</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.cpp" line="66"/>
