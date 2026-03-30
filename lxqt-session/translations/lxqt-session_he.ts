2e1f3479bcc642f141dbbd0858acb823e95b9c7a
Weblate <noreply@weblate.org>
2026-03-30 22:24:26 +0200

    Weblate commits
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/he/
    Translation: LXQt Configuration/lxqt-session

--- lxqt-session/translations/lxqt-session_he.ts
+++ lxqt-session/translations/lxqt-session_he.ts
@@ -53,12 +53,12 @@
     <message>
         <location filename="../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;ברוך בואך ל־LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;נא לבחור את מנהל החלונות כברירת המחדל שלך:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="71"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;You will be able to change this at any time through &lt;span style=&quot;font-weight:600;&quot;&gt;LXQt Settings → Session Settings → X11 Settings&lt;/span&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;אפשר לשנות את זה בכל עת דרך &lt;span style=&quot;font-weight:600;&quot;&gt;הגדרות LXQt ← הגדרות הפעלה ← הגדרות X11&lt;/span&gt;.</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.cpp" line="64"/>
