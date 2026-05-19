99d53b5d1d74f8d0730a5fa6db34d08d4d7b2031
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2026-05-19 16:35:45 +0200

    Weblate commits (#638)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session-menu-entry/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session-menu-entry/sr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-session/sr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-leave-dialog/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-lock-screen/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-logout/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-reboot/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-leave-menu-entry-shutdown/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-session/sr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/xscreensaver-module-name/sr/
    Translation: LXQt Configuration/XScreenSaver Autostart Name
    Translation: LXQt Configuration/lxqt-config-session
    Translation: LXQt Configuration/lxqt-config-session Menu Entry
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Leave Dialog
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Lock Screen
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Logout
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Reboot
    Translation: LXQt Configuration/lxqt-leave Menu Entry: Shutdown
    Translation: LXQt Configuration/lxqt-session
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: Demian <Demian@gmx.co.uk>
    Co-authored-by: Stribor <gejmermi@gmail.com>
    Co-authored-by: Марко Костић <marko.m.kostic@gmail.com>

--- lxqt-session/translations/lxqt-session_de.ts
+++ lxqt-session/translations/lxqt-session_de.ts
@@ -53,12 +53,12 @@
     <message>
         <location filename="../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Willkommen bei LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Bitte wählen Sie Ihren standardmäßigen Fenstermanager aus:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="71"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;You will be able to change this at any time through &lt;span style=&quot;font-weight:600;&quot;&gt;LXQt Settings → Session Settings → X11 Settings&lt;/span&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sie können dies jederzeit unter &lt;span style=&quot;font-weight:600;&quot;&gt;LXQt-Einstellungen → Sitzungseinstellungen → X11-Einstellungen&lt;/span&gt; ändern.</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.cpp" line="64"/>
--- lxqt-session/translations/lxqt-session_sr_RS.ts
+++ lxqt-session/translations/lxqt-session_sr_RS.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/lxqtmodman.cpp" line="347"/>
         <source>Crash Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Извештај рушења</translation>
     </message>
     <message>
         <location filename="../src/lxqtmodman.cpp" line="348"/>
         <source>&lt;b&gt;%1&lt;/b&gt; crashed too many times. Its autorestart has been disabled until next login.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; је пао превише пута. Самостално поновно покретање је онемогућено до следећег пријављивања.</translation>
     </message>
 </context>
 <context>
@@ -19,28 +19,28 @@
     <message>
         <location filename="../src/main.cpp" line="62"/>
         <source>Configuration file path.</source>
-        <translation>Путања конфигурационг фајла.</translation>
+        <translation>Путања датотеке поставки.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="62"/>
         <location filename="../src/main.cpp" line="63"/>
         <source>file</source>
-        <translation>фајл</translation>
+        <translation>датотека</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="63"/>
         <source>Window manager to use.</source>
-        <translation>Менаџер прозора за коришћење.</translation>
+        <translation>Управник прозора за коришћење.</translation>
     </message>
     <message>
         <location filename="../src/sessionapplication.cpp" line="89"/>
         <source>DBus Environment</source>
-        <translation type="unfinished"></translation>
+        <translation>Окружење Д-сабирнице</translation>
     </message>
     <message>
         <location filename="../src/sessionapplication.cpp" line="90"/>
         <source>The DBus Activation Environment wasn&apos;t updated. Some apps might not work properly</source>
-        <translation type="unfinished"></translation>
+        <translation>Окружење за покретање Д-сабирнице није ажурирано. Неки програми можда неће радити исправно</translation>
     </message>
 </context>
 <context>
@@ -48,27 +48,27 @@
     <message>
         <location filename="../src/wmselectdialog.ui" line="14"/>
         <source>Welcome to LXQt</source>
-        <translation>Добро дошли у Рејзор-кјут</translation>
+        <translation>Добро дошли у ЛиксКјут</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Welcome to LXQt&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Please select your default Window Manager:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Добро дошли у ЛиксКјут&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Молимо вас да изаберете свог подразумеваног управника прозора:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.ui" line="71"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;You will be able to change this at any time through &lt;span style=&quot;font-weight:600;&quot;&gt;LXQt Settings → Session Settings → X11 Settings&lt;/span&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Ово ћете моћи да промените у било ком тренутку путем &lt;span style=&quot;font-weight:600;&quot;&gt;Подешавања ЛиксКјута → Подешавања сесије → Подешавања Икс-11&lt;/span&gt;.</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.cpp" line="64"/>
         <source>Other ...</source>
-        <translation>Остало ...</translation>
+        <translation>Друго ...</translation>
     </message>
     <message>
         <location filename="../src/wmselectdialog.cpp" line="65"/>
         <source>Choose your favorite one.</source>
-        <translation>Изабери свој омиљени.</translation>
+        <translation>Изаберите свој омиљени.</translation>
     </message>
 </context>
 </TS>
